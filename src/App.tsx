6e6ee7af55e99e92eace3ab3d21847c7dc356ed2
DANBAM <danbam1709@gmail.com>
2025-01-24 10:56:35 +0900

    App.tsx 테스트용 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Test from "./editor/Test";
+import Home from "./layout/Home.tsx";
 
 function App() {
   return (
     <Router>
         <Routes>
             <Route path={"/"} element={<Test />} />
+            <Route path={"/test"} element={<Home />} />
         </Routes>
     </Router>
   )
