03c10eaddd71b867e0035b37112ae4e139da6f42
DANBAM <danbam1709@gmail.com>
2025-01-31 12:50:19 +0900

    dynamicStyle.ts 파일 삭제

--- src/shared/mixins/dynamicStyle.ts
+++
@@ -1,9 +0,0 @@
-import {css} from "styled-components";
-
-export interface  StyledComponent {
-    $customStyles?: string
-}
-
-export const dynamicStyles = css<StyledComponent>`
-    ${({$customStyles}) => $customStyles ?? ''}
-`;
