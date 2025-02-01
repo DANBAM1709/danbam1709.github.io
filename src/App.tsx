26b24aaf410caf980bdfd6e06bfba8ccf8c00ba5
DANBAM <danbam1709@gmail.com>
2025-02-02 06:20:47 +0900

    App.tsx 기본 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomRichEditor from "./layout/CustomRichEditor.tsx";
+import styled from "styled-components";
+import NavSidebar from "./common/NavSidebar.tsx";
+import Header from "./common/Header.tsx";
+
+const Container = styled.div`
+    display: flex;
+    min-height: 100vh;
+    
+    .content {
+        min-height: calc(100vh - var(--header-height) - var(--footer-height));
+        width: 708px; // 임시
+        margin: 0 auto;
+        background: white;
+    }
+`
 
 function App() {
-  return (
-    <Router>
-        <Routes>
-            <Route path={"/"} element={<CustomRichEditor />} />
-        </Routes>
-    </Router>
-  )
+  return (<Container>
+      <NavSidebar />
+      <div style={{flex: 1}}>
+          <Header />
+          <div className={'content'}>
+            <Router>
+                <Routes>
+                    <Route path={"/"} element={<CustomRichEditor />} />
+                </Routes>
+            </Router>
+          </div>
+          <div style={{height: 'var(--footer-height)'}}>푸터용</div>
+      </div>
+  </Container>)
 }
 
 export default App
