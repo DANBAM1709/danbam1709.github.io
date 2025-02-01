fab58f6a045a71e048b4ee68c7f3f403df81c626
DANBAM <danbam1709@gmail.com>
2025-02-02 06:22:56 +0900

    NavSidebar.tsx 좌측 사이드바 파일 추가

---
+++ src/common/NavSidebar.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 100vh;
+    max-height: 100vh;
+    width: 240px;
+    box-shadow: rgba(0, 0, 0, 0.024) -1px 0 0 0 inset;
+    background: rgb(248, 248, 247);
+    z-index: 10;
+`
+
+interface MenuBtnProps {
+    $depth?: number
+}
+
+const MenuBtn = styled.div<MenuBtnProps>`
+    display: flex;
+    align-items: center;
+    min-height: 28px;
+    line-height: 1.2;
+    user-select: none;
+    font-size: 14px;
+    padding-left: ${({$depth}: MenuBtnProps) => $depth ? `${10 + 8*$depth}px` : '10px'};
+    padding-right: 12px;
+    margin-right: 4px;
+    margin-left: 4px;
+    cursor: pointer;
+    border-radius: 6px;
+    
+    &:hover {
+        background: rgba(55, 53, 47, 0.06);   
+    }
+    
+    img {
+        margin-right: 8px;
+    }
+`
+
+const NavSidebar = () => {
+    return (<Container>
+        <MenuBtn>
+            <img src={'search.svg'} alt={'search.svg'} width={'20px'} height={'20px'} />
+            <span>검색</span>
+        </MenuBtn>
+        <MenuBtn $depth={1}>
+            <img src={'search.svg'} alt={'search.svg'} width={'20px'} height={'20px'} />
+            <span>검색</span>
+        </MenuBtn>
+    </Container>)
+}
+
+export default NavSidebar
