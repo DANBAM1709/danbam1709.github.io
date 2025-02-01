b882403e22cfe4c96fd20533ac683e21cf3382bf
DANBAM <danbam1709@gmail.com>
2025-02-02 06:26:50 +0900

    CustomRichEditor.tsx 기본 레이아웃을 위한 css App.tsx 로 이동

--- src/layout/CustomRichEditor.tsx
+++ src/layout/CustomRichEditor.tsx
@@ -1,34 +1,14 @@
 import styled from "styled-components";
-import ItemButton from "../shared/components/ItemButton.tsx";
-import {Select, Options, Option, DropButton} from "../shared/components/Select.tsx";
-import PlusButton from "../shared/components/PlusButton.tsx";
+import TextEditModule from "../editor/TextEditModule.tsx";
 
 const Container = styled.div`
-    min-height: 100vh;
-    background: #747bff;
-    padding: 0 15rem; // 가변적으로 설정해야 하나...
-    
-    .content {
-        min-height: 100vh;
-        background: #f9f9f9;
-    }    
+    background: yellow;
 `
 
 const CustomRichEditor = () => {
     return (
         <Container>
-            <div className={'content'}>
-                <PlusButton />
-                <ItemButton onClick={() => console.log('히히')} icon={<img src={''} alt={''} />} />
-                <Select>
-                    <DropButton>히</DropButton>
-                    <Options>
-                        <div>설명이용</div>
-                        <Option>안녕하세요 반가워요 잘있어요 다시 만나요</Option>
-                        <Option>히잉</Option>
-                    </Options>
-                </Select>
-            </div>
+            <TextEditModule />
         </Container>
     )
 }
