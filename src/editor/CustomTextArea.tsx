f61c627c2e3c5cf26f175a6031d5499ecf4eab41
DANBAM <danbam1709@gmail.com>
2025-02-02 06:26:15 +0900

    CustomTextArea.tsx 불필요한 css제거 및 margin-bottom 추가(수정 가능)

--- src/editor/CustomTextArea.tsx
+++ src/editor/CustomTextArea.tsx
@@ -2,15 +2,12 @@ import styled from 'styled-components';
 import {KeyboardEvent, ComponentPropsWithoutRef} from "react";
 
 const Container = styled.div`
-    /* 테스트용 css */
-    font-size: 25px;
-    
-    /* 실제 반영할 css  */
     flex: 1;
-    min-width: 0; // 내부 콘텐츠가 넘쳐 길어지는 것을 방지함
+    min-width: 0; // flex: 1 일 때  내부 콘텐츠가 넘쳐 너비가 길어지는 것을 방지함
     width: 100%;
     white-space: pre-wrap;
     display: inline-block; // block 의 경우 엔터 기본 이벤트 규칙이 다소 엉망임..
+    margin-bottom: 4px; // 보류
     
     &:focus {
         outline: none;
@@ -18,7 +15,6 @@ const Container = styled.div`
 `
 
 const CustomTextArea = ({...rest}: ComponentPropsWithoutRef<'div'>) => {
-
     const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => { // 키보드 이벤트
         if (e.key === 'Tab') { // Tab -> /t
             e.preventDefault()
