bb885f8d0e29c5108e3dfbb60038e54db115e980
DANBAM <danbam1709@gmail.com>
2025-02-07 17:59:48 +0900

    ActionToolWrapper.tsx -> PostBlock.tsx 파일명 변경 textarea 에 적용할 이벤트 적용

--- src/editor/PostBlock.tsx
+++ src/editor/PostBlock.tsx
@@ -1,7 +1,8 @@
-import {ReactElement} from "react";
+import {cloneElement, ComponentPropsWithoutRef, KeyboardEvent, ReactElement, useRef} from "react";
 import TooltipWrapper from "../common/ui/TooltipWrapper.tsx";
 import SoftBtn from "../common/ui/SoftBtn.tsx";
 import styled from "styled-components";
+import {useSelection} from "../global/hook.ts";
 
 const Container = styled.div`
     position: relative;
@@ -46,19 +47,88 @@ const Container = styled.div`
     //}
 `
 
-const ActionToolWrapper = ({children}: {children: ReactElement}) => {
-    return (<Container>
+const PostBlock = ({children, ...props}: {children: ReactElement} & ComponentPropsWithoutRef<'div'>) => {
+    const selection = useSelection()
+    const containerRef = useRef<HTMLDivElement|null>(null)
+
+    const handleAddBlock = {
+        onClick: () => {
+            // const parent = containerRef.current!.parentElement as Element
+            // createPortal(<PostBlock><CommonTextArea /></PostBlock>, parent)
+        }
+    }
+
+    const handleEditor = { // CustomTextArea 이벤트
+        onKeyDown: (e: KeyboardEvent<HTMLDivElement>) => {
+            const con = { // 조건 모음
+                empty: !e.currentTarget.firstChild, // 내용이 없다면
+                delete: e.key === 'Backspace'  && e.currentTarget.innerHTML === '<br>', // 블록 삭제
+                up: e.key === 'ArrowUp', // 블록 이동 위로
+                down: e.key === 'ArrowDown', // 블록 이동 아래로
+            }
+
+            // 첫 줄 텍스트 노드 방지
+            if (con.empty) {
+                const range = selection!.getRangeAt(0)
+                const div = document.createElement('div')
+                div.textContent = '\u200B'
+                e.currentTarget.appendChild(div)
+                range.selectNodeContents(div.firstChild!)
+                range.collapse(false) // 마지막 위치로 이동
+            }
+
+            // <- backspace && empty
+            if (con.delete) {
+                e.currentTarget.innerHTML = '<div></div>' // 텍스트 블록 삭제
+            }
+
+            // 방향키 Up
+            if (con.up) {
+                const range = selection!.getRangeAt(0)
+
+                const rangeTop = range.getBoundingClientRect().top
+                const top = e.currentTarget.getBoundingClientRect().top
+
+                const prev = range.startContainer.previousSibling
+
+                const empty = !prev || (e.currentTarget === range.startContainer)
+                const condition1 = rangeTop === 0 && empty;
+                const condition2 = rangeTop !== 0 && (rangeTop - top) < 5 // 커서와 끝의 오차 임의값 5
+
+                if (condition1 || condition2) {
+                    console.log('위로')
+                }
+            }
+
+            // 방향키 Down
+            if (con.down) {
+                const range = selection!.getRangeAt(0)
+
+                const rangeBottom = range.getBoundingClientRect().bottom
+                const bottom = e.currentTarget.getBoundingClientRect().bottom
+
+                const condition1 = rangeBottom === 0 && !range.endContainer.nextSibling // 마지막 줄이면서 내용물 없을 때 Br 태그를 가리키며 rangeBottom=0 이 되므로
+                const condition2 = (bottom - rangeBottom) < 5 // 커서와 끝의 오차 임의값 5
+
+                if (condition1 || condition2) {
+                    console.log('아래로')
+                }
+            }
+        },
+    }
+
+    return (<Container ref={containerRef} {...props}>
         <div className={'action-wrapper'}>
             <div className={'action-group'}>
                 <TooltipWrapper>
-                    <SoftBtn className={'plus-btn'}>
+                    <SoftBtn className={'plus-btn'} {...handleAddBlock}>
                     <img src={`plus.svg`} alt={'plus.svg'} width={'16px'} height={'16px'} />
                     </SoftBtn>
                 </TooltipWrapper>
             </div>
         </div>
-        {children}
-        {/*{cloneElement(children, {ref: contentRef})}*/}
+        {/*{children}*/}
+        {cloneElement(children, handleEditor)}
     </Container>)
 }
 
@@ -151,4 +221,4 @@ const ActionToolWrapper = ({children}: {children: ReactElement}) => {
 //     </Container>)
 // }
 //
-export default ActionToolWrapper
+export default PostBlock
