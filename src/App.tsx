0e9e660fa51d99db8654e142ae1e25ae757a449a
DANBAM <danbam1709@gmail.com>
2025-02-14 09:15:47 +0900

    App.tsx content 클래스 -> Base style 로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,28 @@ import ErrorFallback from "./error/ErrorFallback.tsx";
 import NotFound from "./error/NotFound.tsx";
 import GlobalProvider from "./global/GlobalProvider.tsx";
 import NavSidebar from "./layout/NavSidebar.tsx";
+import Test from "./Test.tsx";
+import RichEditor from "./layout/RichEditor.tsx";
 
 const Container = styled.div`
     display: flex;
     min-height: 100vh;
-    
-    .layout-contents {
-        min-height: 100vh;
-        margin: 0 auto;
-        background: white;
-        padding: 0 50px;
-    }
 `
 
 function App() {
 
     return (<GlobalProvider><Container>
-        <NavSidebar />
-        <div style={{flex: 1}}>
-            <Header />
-            <div className={'layout-contents'}>
-                <BrowserRouter>
-                        <ErrorBoundary FallbackComponent={ErrorFallback}>
-                        <Routes>
-                            <Route path={"/"} element={<CustomRichEditor />} />
-                            <Route path={'*'} element={<NotFound />} />
-                        </Routes>
-                        </ErrorBoundary>
-                </BrowserRouter>
-            </div>
-        </div>
+        {/*<NavSidebar />*/}
+        {/*<Header />*/}
+        <BrowserRouter>
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+                <Routes>
+                    <Route path={"/"} element={<CustomRichEditor />} />
+                    <Route path={"/test"} element={<RichEditor />} />
+                    <Route path={'*'} element={<NotFound />} />
+                </Routes>
+            </ErrorBoundary>
+        </BrowserRouter>
     </Container></GlobalProvider>)
 }
 
