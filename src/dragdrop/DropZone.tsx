f5bd99d6159910bcbd4dbc50a8528369e2f586dc
DANBAM <danbam1709@gmail.com>
2025-02-14 20:14:25 +0900

    DropZone.tsx DragOver 영역과 DropArea 수정

---
+++ src/dragdrop/DropZone.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import {ComponentPropsWithoutRef, MouseEvent, ReactNode, useContext, useMemo} from "react";
+import DropContext from "./DropContext.tsx";
+
+const DropArea = styled.div`
+    user-select: none;
+    display: contents;
+    cursor: grabbing;
+    
+    * {
+        cursor: grabbing !important;
+    }
+    > * {
+        position: relative;
+        z-index: 9998;
+    }
+`
+
+const DragOverArea = styled.div`
+    position: fixed;
+    z-index: 9997;
+    user-select: none;
+    cursor: move;
+    pointer-events: auto !important;
+    top: 0; left: 0; right: 0; bottom: 0;
+`
+const DropZone = ({children, ...rest}: {children?: ReactNode} & ComponentPropsWithoutRef<'div'>) => {
+    const {isDrag, GhostImage, handleDragOver, handlerDragOut, handleDragEnd, handleDrop, handleWindowEnter} = useContext(DropContext)
+
+    const onDragOver = useMemo(() => {
+        return {
+            onMouseEnter: handleWindowEnter,
+            onMouseUp: handleDragEnd,
+            onMouseMove: handlerDragOut,
+        }
+    }, [handleDragEnd, handleWindowEnter, handlerDragOut])
+
+    const onDrop = useMemo(() => {
+        return {
+            onMouseEnter: handleWindowEnter,
+            onMouseUp: (e: MouseEvent<HTMLElement>) => {
+                handleDrop(e)
+                handleDragEnd(e)
+            },
+            onMouseMove: handleDragOver
+        }
+    }, [handleDragEnd, handleDragOver, handleDrop, handleWindowEnter])
+
+    return (<>
+        {isDrag? <>
+                <DragOverArea {...onDragOver}></DragOverArea>
+                <DropArea {...onDrop} {...rest}>{children}</DropArea>
+                <GhostImage className={'ghost-image'} />
+            </>
+            : children}
+    </>)
+}
+
+export default DropZone
