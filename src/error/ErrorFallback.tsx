ee4afba343d2ec04b4a3588efc467b6861af8f69
DANBAM <danbam1709@gmail.com>
2025-02-14 09:11:59 +0900

    ErrorFallback.tsx 새로고침 설정 추가

--- src/error/ErrorFallback.tsx
+++ src/error/ErrorFallback.tsx
@@ -1,15 +1,13 @@
 import {FallbackProps} from "react-error-boundary";
-import {useNavigate} from "react-router-dom";
 import errorStyle from "./errorStyle.ts";
 
 // 런타임 에러 처리용
 const ErrorFallback = ({error}: Readonly<FallbackProps>) => {
-    const navigate = useNavigate();
 
     return (
         <div style={errorStyle}>
             <div>{error.message}</div> <br />
-            <button onClick={()=>navigate(-1)}>이전 페이지로 돌아가기</button>
+            <button onClick={()=>window.location.reload()}>새로고침</button>
         </div>
     );
 }
