97ba28bf22a17171109cac29a7182c20118943bb
DANBAM <danbam1709@gmail.com>
2025-02-14 09:08:50 +0900

    Button.tsx 버튼 base style 정의

---
+++ src/base/Button.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.div.attrs({ tabIndex: 0 })`
+    user-select: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    vertical-align: middle;
+`
+
+export default Button
