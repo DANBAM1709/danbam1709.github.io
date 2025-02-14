5282d214762572b778513a2bb71a3952ad9feaad
DANBAM <danbam1709@gmail.com>
2025-02-14 20:09:31 +0900

    useDrop.tsx image 스타일 분리, src 로딩 전 이미지 투명 적용

--- src/dragdrop/useDrop.tsx
+++ src/dragdrop/useDrop.tsx
@@ -1,56 +1,55 @@
-// -------------------- useDrop --------------------
-import {
-    useState,
-    MouseEvent,
-    MutableRefObject,
-    CSSProperties,
-    useMemo,
-    ComponentProps, ReactElement,
-} from "react";
+import {ComponentProps, CSSProperties, MouseEvent, ReactElement, useState,} from "react";
 import html2canvas from "html2canvas";
 import {DropContextType} from "./DropContext.tsx";
+import styled from "styled-components";
+
+const GhostImageStyle = styled.img`
+    position: fixed;
+    opacity: 0.6;
+    object-fit: contain;
+    z-index: 99999;
+    pointer-events: none !important;
+    user-select: none !important;
+`
 
 interface DragHandler {
-    dropTarget: MutableRefObject<HTMLElement|null>
+    dropTarget: HTMLElement | null
     onDragStart: (e?: MouseEvent<HTMLElement>)=>void
     onDragOver?: (e?: MouseEvent<HTMLElement>)=>void
     onDragOut?: (e?: MouseEvent<HTMLElement>)=>void
     onDrop: (e?: MouseEvent<HTMLElement>)=>void
 }
 
-// dropTarget: 드롭 장소, onDragStart: 드롭 시작, onDragOver: 드롭 중, onDragOut: 드롭 영역 밖, onDrop: 드롭 완료
+// dropTarget: 드롭 장소, onDragStart: 드롭 시작, onDragOver: 드롭 중, onDragOut: 드롭 영역 밖, onDrop: 드롭 완료 || 함수 정의시 (e?: MouseEvent<HTMLElement>)
 const useDrop = ({dropTarget, onDragStart, onDragOver, onDragOut, onDrop}: DragHandler): DropContextType => {
     const [isDrag, setIsDrag] = useState<boolean>(false)
-    const [ghostSrc, setGhostSrc] = useState<string>('') // 고스트 이미지 캡쳐 src
-    const [ghostPos, setGhostPos] = useState<{x: number, y: number}>({x: 0, y: 0})
-    const ghostStyle: CSSProperties = useMemo(() => {
-        return {
-            position: 'fixed',
-            top: ghostPos.y,
-            left: ghostPos.x,
-            pointEvents: 'none',
-            zIndex: 110,
-        }
-    }, [ghostPos])
+    const [ghostSrc, setGhostSrc] = useState<string|null>(null) // 고스트 이미지 캡쳐 src
+    const [ghostStyle, setGhostStyle] = useState<CSSProperties>({})
 
     const getGhostSrc = async () => {
-        const canvas = await html2canvas(dropTarget.current!, {scale: 1})
+        if (!dropTarget) return null
+        const canvas = await html2canvas(dropTarget, {scale: 1})
         return canvas.toDataURL()
     }
     const setGhostPosFunc = (e: MouseEvent<HTMLElement>) => {
-        setGhostPos({x: e.clientX + 5, y: e.clientY + 5})
+        setGhostStyle({
+            top: e.clientY + 5,
+            left: e.clientX + 5,
+        })
     }
 
     // DragStart
     const handleDragStart = (e: MouseEvent<HTMLElement>) => {
         e.preventDefault()
         getGhostSrc().then(src => setGhostSrc(src))
-        onDragStart(e||undefined)
+        onDragStart(e)
         setIsDrag(true)
     }
 
     // DragOver
     const handleDragOver = (e: MouseEvent<HTMLElement>) => {
+        e.stopPropagation()
+        e.preventDefault()
         if (onDragOver) onDragOver(e)
         setGhostPosFunc(e)
     }
@@ -84,7 +83,9 @@ const useDrop = ({dropTarget, onDragStart, onDragOver, onDragOut, onDrop}: DragH
 
     // GhostImage 컴포
     const GhostImage = (props?: ComponentProps<'img'>): ReactElement => {
-        return <img src={ghostSrc} style={ghostStyle} alt={'ghost-image'} {...props} />
+        const transparentImg = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+
+        return (<GhostImageStyle src={ghostSrc ?? transparentImg} alt={'ghost-image'} style={ghostStyle} {...props} />)
     }
 
     return {isDrag, GhostImage, handleDragStart, handleDragOver, handlerDragOut, handleDragEnd, handleDrop, handleWindowEnter}
