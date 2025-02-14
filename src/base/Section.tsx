79b35f3095c3928ab8665184ff2464e4d8434bf8
DANBAM <danbam1709@gmail.com>
2025-02-14 09:16:26 +0900

    Section.tsx 공통 스타일 정의

---
+++ src/base/Section.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Section = styled.div.attrs({ tabIndex: 0 })`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: fit-content;
+    box-sizing: border-box;
+    width: 100%;
+`
+
+export default Section
