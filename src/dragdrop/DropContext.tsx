c0037b95e04d45671f4602f9619da682769bf74e
DANBAM <danbam1709@gmail.com>
2025-02-15 19:26:39 +0900

    DropContext.tsx 공유 데이터 구조 변경

--- src/dragdrop/DropContext.tsx
+++ src/dragdrop/DropContext.tsx
@@ -1,25 +1,52 @@
 import {ComponentPropsWithoutRef, createContext, MouseEvent, ReactElement} from "react";
 
+interface WindowEventHandlers {
+    onMouseEnter: (e: MouseEvent<HTMLElement>)=>void
+    onMouseUp: (e: MouseEvent<HTMLElement>)=>void
+    onMouseMove: (e: MouseEvent<HTMLElement>)=>void
+}
+const defaultWindowEventHandlers: WindowEventHandlers = {
+    onMouseEnter: () => {},
+    onMouseUp: () => {},
+    onMouseMove: () => {},
+}
+
+interface DropEventHandlers {
+    onMouseEnter: (e: MouseEvent<HTMLElement>)=>void
+    onMouseUp: (e: MouseEvent<HTMLElement>)=>void
+    onMouseMove: (e: MouseEvent<HTMLElement>)=>void
+}
+const defaultDropEventHandlers: DropEventHandlers = {
+    onMouseEnter: () => {},
+    onMouseUp: () => {},
+    onMouseMove: () => {},
+}
+
+interface DragStartEventHandlers {
+    onMouseDown: (e: MouseEvent<HTMLElement>)=>void
+    onMouseUp: (e: MouseEvent<HTMLElement>)=>void
+    onMouseLeave: (e: MouseEvent<HTMLElement>)=>void
+}
+const defaultDragStartEventHandlers: DragStartEventHandlers = {
+    onMouseDown: () => {},
+    onMouseUp: () => {},
+    onMouseLeave: () => {},
+}
+
 export interface DropContextType {
     isDrag: boolean
     GhostImage: (props?: ComponentPropsWithoutRef<'img'>) => ReactElement
-    handleDragStart: (e: MouseEvent<HTMLElement>)=>void
-    handleDragOver: (e: MouseEvent<HTMLElement>)=>void
-    handlerDragOut: (e: MouseEvent<HTMLElement>)=>void
-    handleDragEnd: (e: MouseEvent<HTMLElement>)=>void
-    handleDrop: (e: MouseEvent<HTMLElement>)=>void
-    handleWindowEnter: (e: MouseEvent<HTMLElement>)=>void
+    handleWindowEvent: WindowEventHandlers
+    handleDropEvent: DropEventHandlers
+    handleDragStartEvent: DragStartEventHandlers
 }
 
 const DropContext =  createContext<DropContextType>({
     isDrag: false,
     GhostImage: () => <></>,
-    handleDragStart: () => {},
-    handleDragOver: () => {},
-    handlerDragOut: () => {},
-    handleDragEnd: () => {},
-    handleDrop: () => {},
-    handleWindowEnter: () => {},
+    handleWindowEvent: defaultWindowEventHandlers,
+    handleDropEvent: defaultDropEventHandlers,
+    handleDragStartEvent: defaultDragStartEventHandlers
 })
 
 export default DropContext
