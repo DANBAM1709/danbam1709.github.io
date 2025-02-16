f517d944ac0285dea35b45f85b4b9b642515e966
DANBAM <danbam1709@gmail.com>
2025-02-16 10:15:26 +0900

    Select.tsx -> SelectProvider.tsx 파일명 변경

---
+++ src/select/SelectProvider.tsx
@@ -0,0 +1,22 @@
+import {ComponentPropsWithoutRef, useMemo, useState} from "react";
+import SelectContext from "./SelectContext.ts";
+import styled from "styled-components";
+
+const Container = styled.div`
+    user-select: none;
+`
+
+// <Select><DropButton /><Options><Option /></Options></Select>
+const SelectProvider = (props: ComponentPropsWithoutRef<'div'>) => {
+    const [open, setOpen] = useState<boolean>(false)
+    const [buttonEl, setButtonEl] = useState<HTMLElement|null>(null)
+
+    // 리랜더링될 때마다 새로운 객체로 생성되는 것 방지
+    const value = useMemo(() => ({open, setOpen, buttonEl, setButtonEl}), [open, setOpen, buttonEl, setButtonEl])
+
+    return (
+        <SelectContext.Provider value={value} ><Container {...props} /></SelectContext.Provider>
+    )
+}
+
+export default SelectProvider
