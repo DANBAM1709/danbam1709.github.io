1ea4c129bed3a3282ca1056576e2c8a558345d79
DANBAM <danbam1709@gmail.com>
2025-02-16 10:13:12 +0900

    Options.tsx 호버 js -> css

--- src/select/Options.tsx
+++ src/select/Options.tsx
@@ -1,53 +1,79 @@
-import {ComponentPropsWithoutRef, useContext, useEffect, useRef} from "react";
+import {ComponentPropsWithoutRef, useContext, useEffect, useState} from "react";
 import SelectContext from "./SelectContext.ts";
 import styled from "styled-components";
+import {usePopper} from "react-popper";
+import {Placement} from "@popperjs/core";
 
-const Container = styled.div.attrs({ tabIndex: 0 })` // tabIndex: 0 -> focus ok    
+const Container = styled.div.attrs({tabIndex: 0})`
+    // 기능
+    pointer-events: none;
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+    
+    &:focus-within {
+        z-index: 15;
+        opacity: 1;
+        pointer-events: auto;
+    }
+    
+    // 기본 스타일
     display: flex;
     flex-direction: column;
     background: white;
-    position: absolute;
-    top: 4px;
-    opacity: 0;
     gap: 1px;
     padding: 4px 0;
     max-height: 70vh;
     box-shadow: rgba(15, 15, 15, 0.05) 0 0 0 1px, rgba(15, 15, 15, 0.1) 0 3px 6px, rgba(15, 15, 15, 0.2) 0 9px 24px;
     border-radius: 10px;
-    
-    & > div:not([class]) { // className 없는 div
-        white-space: nowrap;
-        padding: 0 14px;
-        margin: 6px 0 4px;
-        color: rgba(55, 53, 47, 0.65);
-    }
-    &:focus {
-        outline: none;
-    }
+    top: 20px;
 `
 
-// <Options><Option /></Options>
-const Options = (props: ComponentPropsWithoutRef<'div'>) => {
-    const {open, setOpen} = useContext(SelectContext)
-    const target = useRef<HTMLDivElement>(null)
+interface Props extends ComponentPropsWithoutRef<'div'> {
+    direction?: string
+    offset?: number
+}
+
+// <Options direction='right' offset=10><Option /></Options>
+const Options = ({direction, offset, ...props}: Props) => {
+    const {open, setOpen, buttonEl} = useContext(SelectContext)
+    const [target, setTarget] = useState<HTMLElement|null>(null)
+
+    const {styles, attributes} = usePopper(buttonEl, target,  {
+        placement: direction as Placement ?? 'right',
+        modifiers: [
+            {
+                name: 'preventOverflow',
+                options: {
+                    boundary: document.documentElement,
+                },
+            },
+            {
+                name: 'offset',
+                options: {
+                    offset: [0, offset ?? 10], // [skidding, distance]: skidding은 기준 요소를 따라 좌우 이동, distance는 기준 요소와의 거리
+                },
+            },
+            {
+                name: 'flip', // 화면에 맞게 방향 전환
+                options: {
+                    fallbackPlacements: (['right', 'left', 'top', 'bottom'] as Placement[]).filter(
+                        item => item !== ((direction as Placement) ?? 'right') // 선택 방향 제외 순서대로
+                    ),
+                },
+            },
+        ]
+    })
 
     useEffect(() => {
-        if (!target.current) return
         if (open) {
-            target.current.focus()
-            target.current.style.opacity = '1'
-            target.current.style.zIndex = '10'
-            target.current.style.pointerEvents = 'auto'
+            target?.focus()
         } else {
-            target.current.style.opacity = '0'
-            target.current.style.zIndex = '-10'
-            target.current.style.pointerEvents = 'none'
+            target?.blur()
         }
-    }, [open]);
+    }, [open, target]);
 
-    return (<div style={{position: 'relative'}}>
-        <Container ref={target} onBlur={()=>setOpen(false)} {...props} />
-    </div>)
+    return (<Container ref={setTarget} onBlur={()=>setOpen(false)} style={{...styles.popper}} {...attributes.popper} {...props} />)
 }
 
 export default Options
