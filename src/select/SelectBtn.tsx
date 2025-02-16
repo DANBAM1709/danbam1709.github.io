05baf8a725233effa23e369fadf4fccdddb76535
DANBAM <danbam1709@gmail.com>
2025-02-16 10:14:19 +0900

    DropButton.tsx -> SelectBtn.tsx popper 적용을 위한 ref 설정

---
+++ src/select/SelectBtn.tsx
@@ -0,0 +1,47 @@
+import {
+    ComponentPropsWithoutRef,
+    forwardRef,
+    MouseEvent,
+    MutableRefObject,
+    ReactNode,
+    useCallback,
+    useContext
+} from "react";
+import SelectContext from "./SelectContext.ts";
+import styled from "styled-components";
+import Button from "../base/Button.tsx";
+
+const Container = styled(Button)``
+
+interface Props extends ComponentPropsWithoutRef<"div"> {
+    children: ReactNode
+}
+const SelectBtn = forwardRef<HTMLDivElement, Props>(({children, onClick, ...props}, ref) => {
+    const {setOpen, setButtonEl} = useContext(SelectContext) // Options open
+
+    // 외부 ref와 내부 상태(ref)를 모두 처리하기 위한 콜백
+    const containerRef = useCallback(
+        (node: HTMLDivElement | null) => {
+            setButtonEl(node);
+
+            if (!ref) return;
+            if (typeof ref === 'function') {
+                ref(node);
+            } else {
+                (ref as MutableRefObject<HTMLElement | null>).current = node;
+            }
+        },
+        [ref, setButtonEl]
+    );
+    
+
+    const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+        setOpen(prev => !prev)
+        if(onClick) onClick(e)
+    }
+
+
+    return (<Container ref={containerRef} onClick={handleClick} {...props}>{children}</Container>)
+})
+
+export default SelectBtn
