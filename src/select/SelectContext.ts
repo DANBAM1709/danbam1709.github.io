37ddd52b4742a2f60a7fec0e4040e5e922bf052d
DANBAM <danbam1709@gmail.com>
2025-02-16 10:15:00 +0900

    SelectContext.ts popper 설정을 위한 변수 추가

--- src/select/SelectContext.ts
+++ src/select/SelectContext.ts
@@ -3,11 +3,15 @@ import {createContext, Dispatch, SetStateAction} from "react";
 interface SelectContextType {
     open: boolean, //Options hide|show
     setOpen: Dispatch<SetStateAction<boolean>>
+    buttonEl: HTMLElement|null
+    setButtonEl: Dispatch<SetStateAction<HTMLElement|null>>
 }
 
 const SelectContext = createContext<SelectContextType>({ // Context 내 전역 변수 관리 (컴포넌트 별로 관리 가능)
     open: false,
-    setOpen: () => {} // undefined 로 인한 오류 방지
+    setOpen: () => {}, // undefined 로 인한 오류 방지
+    buttonEl: null,
+    setButtonEl: () => {}
 })
 
 export default SelectContext
