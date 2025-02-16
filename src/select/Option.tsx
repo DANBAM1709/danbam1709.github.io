ef0fdda5b57d7b75c93a9b9c5ea572c78e163d26
DANBAM <danbam1709@gmail.com>
2025-02-16 10:13:35 +0900

    Option.tsx css 설정 변경

--- src/select/Option.tsx
+++ src/select/Option.tsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 import {ComponentPropsWithoutRef, MouseEvent, useContext} from "react";
 import SelectContext from "./SelectContext.ts";
 
-const OptionContainer = styled.div`
-    user-select: none; // 드래그 불가
+const Container = styled.div`
+    display: flex;
+    align-items: center;
     cursor: pointer;
     transition: background 20ms ease-in;
     margin: 0 4px;
@@ -12,11 +13,27 @@ const OptionContainer = styled.div`
     box-sizing: border-box;
     gap: 10px;
     white-space: nowrap;
-    
+
     &:hover {
         background: rgba(55, 53, 47, 0.06);
     }
 `
+
+// const OptionContainer = styled.div`
+//     user-select: none; // 드래그 불가
+//     cursor: pointer;
+//     transition: background 20ms ease-in;
+//     margin: 0 4px;
+//     border-radius: 6px;
+//     padding: 4px 12px 4px 10px;
+//     box-sizing: border-box;
+//     gap: 10px;
+//     white-space: nowrap;
+//
+//     &:hover {
+//         background: rgba(55, 53, 47, 0.06);
+//     }
+// `
 
 const Option = ({onClick, ...props}: ComponentPropsWithoutRef<'div'>) => {
     const {setOpen} = useContext(SelectContext)
@@ -26,7 +43,7 @@ const Option = ({onClick, ...props}: ComponentPropsWithoutRef<'div'>) => {
         if (onClick) onClick(e) // 상위 컴포넌트에서 정의한 onClick
     }
 
-    return (<OptionContainer onClick={handleClick} {...props} />)
+    return (<Container onClick={handleClick} {...props} />)
 }
 
 export default Option
