db8a75838e07b29b2ff41433f836e19560b406c2
DANBAM <danbam1709@gmail.com>
2025-02-17 16:47:41 +0900

    RichEditorUI.ts 드래그 옵션 재정의

--- src/layout/RichEditorUI.ts
+++ src/layout/RichEditorUI.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import Button from "../base/Button.tsx";
+import SelectBtn from "../select/SelectBtn.tsx";
+import Options from "../select/Options.tsx";
+import Option from "../select/Option.tsx";
 
 const DraggableCard = styled.div.attrs({ tabIndex: 0 })`
     width: fit-content;
@@ -16,17 +18,17 @@ const ActionTool = styled.div`
     height: calc(1em * var(--line-height));
     align-items: center;
 `
-const DragButton = styled(Button)`
+const DragButton = styled(SelectBtn)`
     border-radius: 0.125em;
     box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
     border: 1px solid rgba(0, 0, 0, 0.08);
-    transition: background 20ms ease-in, fill 20ms ease-in;
-    fill: rgba(55, 53, 47, 0.4);
+    transition: background 20ms ease-in, color 20ms ease-in;
+    color: rgba(55, 53, 47, 0.4);
     height: 18px;
     
     &:hover {
         background: rgba(55, 53, 47, 0.03);
-        fill: rgba(55, 53, 47, 0.5);
+        color: rgba(55, 53, 47, 0.5);
     }
     
     & > * {
@@ -34,13 +36,13 @@ const DragButton = styled(Button)`
         height: 15px;
     }
 `
-const PlusButton = styled(Button)`
+const PlusButton = styled(SelectBtn)`
     position: relative;
     z-index: 10;
     border-radius: 50%;
     border: 1px solid #3399ff;
     opacity: 0.5;
-    fill: #3399ff;
+    color: #3399ff !important;
     box-sizing: border-box;
     transition: opacity 20ms ease-in;
     padding: 0.05em;
@@ -88,4 +90,23 @@ const BottomDropZone = styled.div`
     z-index: -1;
 `
 
-export {DraggableCard, TopDropZone, BottomDropZone, CardDividerLine, CardDivider, Card, ActionTool, DragButton, PlusButton}
+const DragOptions = styled(Options)`
+    display: flex;
+    flex-direction: column;
+    background: white;
+    gap: 1px;
+    padding: 4px 0;
+    max-height: 70vh;
+    box-shadow: rgba(15, 15, 15, 0.05) 0 0 0 1px, rgba(15, 15, 15, 0.1) 0 3px 6px, rgba(15, 15, 15, 0.2) 0 9px 24px;
+    border-radius: 10px;
+    top: 20px;
+`
+const DragOption = styled(Option)`
+    & > * {
+        width: 20px;
+        height: 20px;
+    }
+    
+`
+
+export {DragOptions, DragOption, DraggableCard, TopDropZone, BottomDropZone, CardDividerLine, CardDivider, Card, ActionTool, DragButton, PlusButton}
