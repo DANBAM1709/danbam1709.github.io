609f34f047e42352f0cd9d9ae4f5dd0650e671af
DANBAM <danbam1709@gmail.com>
2025-02-17 16:46:47 +0900

    RichEditor.tsx Select 메뉴 추가 reducer 적용

--- src/layout/RichEditor.tsx
+++ src/layout/RichEditor.tsx
@@ -11,9 +11,10 @@ import {
     CardDividerLine,
     DragButton,
     DraggableCard,
+    DragOption,
     PlusButton,
     TopDropZone
-} from "../editor/EditorUI.ts";
+} from "./RichEditorUI.ts";
 import {MouseEvent, useEffect, useRef, useState} from "react";
 import TooltipWithComponent from "../common/TooltipWithComponent.tsx";
 import CardSelector, {CardProps} from "../editor/CardSelector.tsx";
@@ -21,7 +22,16 @@ import Draggable from "../dragdrop/Draggable.tsx";
 import DragDropProvider from "../dragdrop/DragDropProvider.tsx";
 import useDrop from "../dragdrop/useDrop.tsx";
 import DropZone from "../dragdrop/DropZone.tsx";
-import EditorProvider from "../editor/EditorProvider.tsx";
+import {useTooltip} from "../global/hook.ts";
+import SelectProvider from "../select/SelectProvider.tsx";
+import Options from "../select/Options.tsx";
+import Comment from '../assets/svg/comment.svg?react'
+import ColorPicker from '../assets/svg/color-picker.svg?react'
+import Trash from '../assets/svg/trash.svg?react'
+import Copy from '../assets/svg/copy.svg?react'
+import Swap from '../assets/svg/swap.svg?react'
+import TextToolbar from "../editor/TextToolbar.tsx";
+import {useRichEditContext} from "../editor/RichEditorReducer.ts";
 
 const Container = styled(MainContainer)`
     font-size: 20px;
@@ -52,11 +62,18 @@ const Container = styled(MainContainer)`
     // }
 `
 
-const CustomRichEditor = () => {
-    const cardRefs = useRef<(HTMLElement|null)[]>([]) // 카드 배열 ref [], 0: 제목
+// 자식 컴포넌트에서 노출할 ref 타입
+export interface GetDataHTMLElement extends HTMLElement {
+    getData: () => string;
+}
+
+const RichEditor = () => {
+    const cardRefs = useRef<(GetDataHTMLElement|null)[]>([]) // 카드 배열 ref [], 0: 제목
     const [cards, setCards] = useState<CardProps[]>([]) // 출력할 데이터
-    const [targetIndex, setTargetIndex] = useState<number>(-1) // 이동할 컴포넌트
-    const [addIndex, setAddIndex] = useState<number>(-1)
+    const [targetIndex, setTargetIndex] = useState<number>(-1) // 이동할 컴포넌트 
+    const [moveIndex, setMoveIndex] = useState<number>(-1) // 이동될 인덱스
+    const tooltip = useTooltip()
+    const {state, dispatch} = useRichEditContext()
 
     useEffect(() => {
         setCards([
@@ -68,38 +85,61 @@ const CustomRichEditor = () => {
         ])
     }, []);
 
+    useEffect(() => { // 카드 삭제
+        const index = cardRefs.current.findIndex(ref => ref === state.deleteEl);
+        if (index > 0) {
+            setCards(card => card.filter((_, i) => i !== index))
+            cardRefs.current[index-1]?.focus()
+        }
+    }, [state.deleteEl]);
+
     // 드래그 앤 드롭 정의
     const handleDrop = useDrop({
         dropTarget: cardRefs.current[targetIndex],
+        onDragStartBefore: (e?: MouseEvent<HTMLElement>) => {
+            console.log('뭐지?')
+            console.log(e?.currentTarget.dataset.targetIndex)
+            const index = parseInt(e?.currentTarget.dataset.targetIndex ?? '0')
+            setTargetIndex(index)
+        },
         onDragOver: (e?: MouseEvent<HTMLElement>) => {
             const index = parseInt(e?.currentTarget.dataset.selectIndex ?? '-1')
-            if (index === targetIndex) {
-                setAddIndex(-1)
-            } else {
-                setAddIndex(index)
-            }
+            setMoveIndex(index)
         },
         onDragOut: () => {
-            setAddIndex(-1)
+            setMoveIndex(-1)
         },
         onDrop: () => {
-            if (addIndex === -1) return // 이동 X
+            if (moveIndex === -1 || moveIndex + 1 === targetIndex) return // 이동 X
 
             setCards(prev => { // 위치 이동
                 const copy = [...prev];
-                copy.splice(addIndex + 1, 0, copy.splice(targetIndex, 1)[0]);
+                const [element] = copy.splice(targetIndex, 1);
+                const insertIndex = targetIndex <= moveIndex ? moveIndex : moveIndex + 1;
+                copy.splice(insertIndex, 0, element);
                 return copy;
             });
         },
     })
 
-    // 드래그 전 위치 확인
-    const handleDragBefore = {
-        onMouseEnter: (e: MouseEvent<HTMLElement>) => {
-            if (handleDrop.isDrag) return // 드리그 중 무시
-            const index = parseInt(e?.currentTarget.dataset.targetIndex ?? '0')
-            setTargetIndex(index)
+    const handleAddCard = (index: number) => ({
+        onClick: () => {
+            const addIndex = index + 1
+            dispatch({type: 'DELETE_CARD'})
         }
+    })
+
+    const onDeleteCard = (index: number) => dispatch({type: 'DELETE_CARD', payload: cardRefs.current[index]!})
+
+    // 카드 데이터 변경
+    const onCardsUpdate = () => {
+        setCards(prev => {
+            return prev.map((card, index) => ({
+                id: card.id,
+                mode: card.mode,
+                data: cardRefs.current[index]?.getData() ?? "", // getData()가 undefined면 빈 문자열을 사용
+            }));
+        });
     }
 
     return (<DragDropProvider useDrop={handleDrop}><Container>
@@ -107,24 +147,40 @@ const CustomRichEditor = () => {
             return (<Section key={card.id} data-lastblock={cards.length === index+1? 'true': undefined}>
                 <DraggableCard>
                     {/* 제목이면 드래그 버튼 제외 */}
-                    {index !== 0? <ActionTool {...handleDragBefore} data-target-index={index}>
-                        <Draggable><TooltipWithComponent Component={<DragButton><Grab /></DragButton>} summary={'쓰읍'} /></Draggable>
+                    {index !== 0? <ActionTool>
+                        <Draggable data-target-index={index}><SelectProvider>
+                                {/* DragBtn = SelectBtn */}
+                                <TooltipWithComponent Component={<DragButton><Grab /></DragButton>} summary={tooltip.editorDragBtn} />
+                                <Options>
+                                    {/* DragOption = Option */}
+                                    <DragOption><Comment />댓글</DragOption>
+                                    <DragOption onClick={()=>onDeleteCard(index)}><Trash />삭제</DragOption>
+                                    <DragOption><Copy />복제</DragOption>
+                                    <DragOption><Swap />전환</DragOption>
+                                    <SelectProvider>
+                                        <DragButton><ColorPicker />색 진심 뭐지</DragButton>
+                                        <Options>
+                                            호엥
+                                        </Options>
+                                    </SelectProvider>
+                                </Options>
+                        </SelectProvider></Draggable>
                     </ActionTool>: null}
                     {/* 카드 선택 */}
-                    <Card ref={el => cardRefs.current[index]=el}><CardSelector mode={card.mode} data={card.data} /></Card>
+                    <Card><CardSelector ref={el => cardRefs.current[index]=el} mode={card.mode} data={card.data} /></Card>
                 </DraggableCard>
-                {/* 카드 나누는 기준 */}
+                {/* 카드 나누는 기준 + 카드 추가 */}
                 <CardDivider>
-                    {addIndex === index? <CardDividerLine/>: null}
-                    <TooltipWithComponent Component={<PlusButton><Plus /></PlusButton>} summary={'클릭해서 블록 추가'} />
+                    {moveIndex === index? <CardDividerLine/>: null}
+                    <TooltipWithComponent Component={<PlusButton {...handleAddCard(index)}><Plus /></PlusButton>} summary={tooltip.editorPlusBtn} />
                 </CardDivider>
+                {/* 드롭 영역 상|하 */}
                 <DropZone data-select-index={index-1}><TopDropZone/></DropZone>
                 <DropZone data-select-index={index}><BottomDropZone/></DropZone>
             </Section>)
         })}
+        {state.isTooltip? <TextToolbar />:null}
     </Container></DragDropProvider>)
 }
-
-const RichEditor = () => (<EditorProvider><CustomRichEditor /></EditorProvider>)
 
 export default RichEditor
