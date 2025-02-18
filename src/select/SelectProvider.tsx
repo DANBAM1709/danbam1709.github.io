e37bb5734736456f26665ff0acaa83aa0bf282bc
DANBAM <danbam1709@gmail.com>
2025-02-18 09:53:51 +0900

    SelectProvider.tsx 컨테이너 삭제

--- src/select/SelectProvider.tsx
+++ src/select/SelectProvider.tsx
@@ -1,13 +1,12 @@
-import {ComponentPropsWithoutRef, useMemo, useState} from "react";
+import {ReactNode, useMemo, useState} from "react";
 import SelectContext from "./SelectContext.ts";
-import styled from "styled-components";
 
-const Container = styled.div.attrs({tabIndex: 0})`
-    user-select: none;
-`
+interface Props {
+    children: ReactNode
+}
 
 // <Select><DropButton /><Options><Option /></Options></Select>
-const SelectProvider = (props: ComponentPropsWithoutRef<'div'>) => {
+const SelectProvider = ({children}: Props) => {
     const [open, setOpen] = useState<boolean>(false)
     const [buttonEl, setButtonEl] = useState<HTMLElement|null>(null)
 
@@ -15,8 +14,39 @@ const SelectProvider = (props: ComponentPropsWithoutRef<'div'>) => {
     const value = useMemo(() => ({open, setOpen, buttonEl, setButtonEl}), [open, buttonEl])
 
     return (
-        <SelectContext.Provider value={value}><Container {...props} /></SelectContext.Provider>
+        <SelectContext.Provider value={value}>{children}</SelectContext.Provider>
     )
 }
+
+// 개발 단계 확인
+// type ExtendedComponentType = JSXElementConstructor<unknown> & {
+//     WrappedComponent?: JSXElementConstructor<unknown>
+// }
+// SelectProvider.propTypes = {
+//     children: function (props: Record<string, unknown>, propName: string, componentName: string) {
+//         const childrenArray = Children.toArray(props[propName] as ReactNode[]);
+//         const selectBtnCount = childrenArray.filter(
+//             (child) => {
+//                 if (isValidElement(child)) {
+//                     console.log(child)
+//                     if (typeof child.type === 'string') return false
+//                     console.log((child.type as ComponentType<unknown>).displayName?.includes('SelectBtn'))
+//                     return (child.type as ComponentType<unknown>).displayName?.includes('SelectBtn')
+//                 }
+//                     // return child.type.displayName === SelectBtn || (child.type as ExtendedComponentType).WrappedComponent === SelectBtn
+//             }
+//         ).length
+//
+//         console.log(selectBtnCount)
+//
+//
+//         // if (selectBtnCount !== 1) {
+//         //     return new Error(
+//         //         `${componentName} 내부에는 반드시 단 하나의 SelectBtn이 있어야 합니다. 현재 ${selectBtnCount}개 있습니다.`
+//         //     );
+//         // }
+//         return null;
+//     },
+// };
 
 export default SelectProvider
