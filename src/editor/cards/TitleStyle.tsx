6c42a6dc64163d2da0bc40c4c6644e29eaa96603
DANBAM <danbam1709@gmail.com>
2025-02-18 09:54:13 +0900

    TitleStyle.tsx 컨테이너 삭제

--- src/editor/cards/TitleStyle.tsx
+++ src/editor/cards/TitleStyle.tsx
@@ -1,16 +1,25 @@
 import styled from "styled-components";
 import TextArea from "../../common/TextArea.tsx";
-import {forwardRef} from "react";
+import {forwardRef, useImperativeHandle, useRef} from "react";
+import {GetDataHTMLElement} from "../../layout/RichEditor.tsx";
 
-const Container = styled.div`
-    padding-top: 80px;
-`
 const Title = styled(TextArea)`
     font-size: 32px;
 `
 
-const TitleStyle = forwardRef<HTMLDivElement, {children: string}>(({children}, ref) => {
-    return (<Container><Title ref={ref}>{children}</Title></Container>)
+const TitleStyle = forwardRef<GetDataHTMLElement, {children: string}>(({children}, ref) => {
+    const targetRef = useRef<HTMLDivElement>(null)
+
+    useImperativeHandle(ref, () => {
+        if (targetRef.current) {
+            return Object.assign(targetRef.current, {
+                getData: () => targetRef.current!.innerHTML
+            }) as GetDataHTMLElement
+        }
+        throw new Error('BasicStyle ref error!')
+    }, []);
+
+    return (<Title ref={targetRef}>{children}</Title>)
 })
 
 export default TitleStyle
