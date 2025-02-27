c4730814ea6b697acde9fb0f603e07ade588e4e9
DANBAM <danbam1709@gmail.com>
2025-02-27 11:42:47 +0900

    useUndoRedo.ts isUndo 시 인덱스 변경되지 않아 업데이트 되지 않던 문제 해결

--- src/common/history/useUndoRedo.ts
+++ src/common/history/useUndoRedo.ts
@@ -7,7 +7,7 @@ useUndoRedo<T> 명시할 것 <br />
 data: 저장할 데이터 변경됨을 인식하고 history 에 저장됨 <br />
 setData: 데이터를 저장할 것을 가져오기 <br />
 getLatestData: () => T[] 최근 데이터 가져오기(데이터만!) <br />
-return {history: data[], index: 현재 위치}
+return {history: data[], index: 현재 위치, canHistoryUpdate: data 변경시 history 추가 가능 여부}
  */
 const useUndoRedo = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatestData: ()=>T) => {
     const [isUndo, setIsUndo] = useState<boolean>(false) // 뒤로
@@ -16,7 +16,6 @@ const useUndoRedo = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatest
     const [history, setHistory] = useState<T[]>([])
     const [index, setIndex] = useState<number>(-1)
     const [canHistoryUpdate, setCanHistoryUpdate] = useState<boolean>(true) // history 추가 가능 여부
-
     const [isLatestIndex, setIsLatestIndex] = useState<boolean>(true)
 
     useEffect(() => {
@@ -27,13 +26,15 @@ const useUndoRedo = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatest
 
         setHistory(prev => {
             const copy = isLatestIndex? [...prev]: prev.slice(0, index+1)
-            const lastRecord = copy[copy.length-1]
+            setIsLatestIndex(true) // 현재 마지막 인덱스 선택
 
+            const lastRecord = copy[copy.length-1]
             if (!isEqual(lastRecord, data)) {
                 copy.push(data)
+            } else {
+                return prev
             }
 
-            setIsLatestIndex(true) // 현재 마지막 인덱스 선택
             return copy
         })
     }, [data])
@@ -45,7 +46,14 @@ const useUndoRedo = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatest
 
     useEffect(() => {
         if (isUndo) {
-            setIndex((history.length-2>=0)? history.length-2: 0)
+            const selectedIndex = (history.length-2>=0)? history.length-2: 0
+            setIndex(prevIndex => {
+                if (prevIndex === selectedIndex) {
+                    setCanHistoryUpdate(false)
+                    setData(history[selectedIndex])
+                }
+                return selectedIndex
+            })
             setIsUndo(false)
         }
         if (isRedo) {
@@ -69,7 +77,7 @@ const useUndoRedo = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatest
     useEffect(() => {
         if (!isUndoRedo) return
 
-        setIsLatestIndex(false) // 아래 history 변경이 아닌 것을 실행시키기 위한 임시 값 어차피 그 아래에 true 가능하게 해뒀음
+        setIsLatestIndex(false) // 아래 index 변경으로 인한 것을 실행시키기 위한 임시 값 어차피 그 아래에 true 가능하게 해뒀음
         if (isUndo) {
             setIndex(preIndex => {
                 const newIndex = preIndex-1
@@ -90,7 +98,7 @@ const useUndoRedo = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatest
     useLayoutEffect(() => { // 인덱스 변경될 경우
         if (index < 0 || history.length === 0) return
 
-        if (!isLatestIndex) { // history 변경으로 인한 것이 아니라면
+        if (!isLatestIndex) { // index 변경으로 인한 것이라면
             setCanHistoryUpdate(false)
             setData(history[index])
         }
@@ -100,7 +108,7 @@ const useUndoRedo = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatest
 
     /* =============== 이벤트 리스너 =============== */
     useEffect(() => {
-        eventManager.addEventListener('keydown', 'useDataManager', (event) => {
+        eventManager.addEventListener('keydown', 'useUndoRedo', (event) => {
             const e = event as KeyboardEvent
             if (e.ctrlKey && e.key.toLowerCase() === 'z' ) {
                 e.preventDefault() // 브라우저 기본 뒤로 가기 방지
@@ -111,9 +119,13 @@ const useUndoRedo = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatest
                 setIsRedo(true)
             }
         })
+
+        return () => {
+            eventManager.removeEventListener('keydown', 'useUndoRedo')
+        }
     }, []);
 
-    return {history, index}
+    return {history, index, canHistoryUpdate}
 }
 
 export default useUndoRedo
