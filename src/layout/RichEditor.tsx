9f8148e7f749bfd936fa97d42c87e7bedbef20b4
DANBAM <danbam1709@gmail.com>
2025-02-28 10:09:42 +0900

    RichEditor.tsx 커스텀 훅 다시 합치는 작업 중 현재 history 작업중

--- src/layout/RichEditor.tsx
+++ src/layout/RichEditor.tsx
@@ -15,7 +15,7 @@ import {
     PlusButton,
     TopDropZone
 } from "./RichEditor.ui.ts";
-import {useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState} from "react";
+import {MouseEvent, useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState} from "react";
 import TooltipWithComponent from "../common/component/TooltipWithComponent.tsx";
 import CardSelector, {CardProps} from "../features/editor/CardSelector.tsx";
 import Draggable from "../common/dragdrop/Draggable.tsx";
@@ -36,6 +36,8 @@ import useHistory from "../common/history/useHistory.ts";
 import {eventManager} from "../global/event.ts";
 import isEqual from "fast-deep-equal";
 import {CustomTextAreaElement, isCustomTextAreaElement} from "../common/component/TextArea.tsx";
+import useRichHistory from "../features/editor/useRichHistory.ts";
+import useDrop from "../common/dragdrop/useDrop.tsx";
 
 const Container = styled(MainContainer)`
     font-size: 20px;
@@ -76,13 +78,22 @@ export interface GetDataHTMLElement extends HTMLElement {
     getData: () => string;
 }
 
-export type StageCardsFunc = (cards: CardProps[]) => CardProps[]
-interface Cursor {
+// history 관련 타입 정의
+type Cursor = {
     startContainer: Node
     startOffset: number
     endContainer: Node
     endOffset: number
+} | null
+interface Scroll {
+    x: number
+    y: number
 }
+type Data = {
+    cards: CardProps[]
+    cursor: Cursor|null
+    scroll: Scroll
+} | null
 
 const RichEditor = () => {
     const cardRefs = useRef<{ [id: string]: GetDataHTMLElement | null }>({}); // 객체를 card.id로 관리
@@ -96,27 +107,121 @@ const RichEditor = () => {
     const {editorDragBtn, editorPlusBtn} = useTooltip()
     const {state: {isTooltip}} = useRichEditorContext()
 
-    const [stagedCardsFunc, setStagedCardsFunc] = useState<StageCardsFunc|null>(null) // 카드 업데이트 방식 function 저장
-    const [canStageUpdate, setCanStageUpdate] = useState(false) // 카드 업데이트 step 나누기 위한 flag
+    /* ========== History 설정 ========== */
+    const selection = useMemo(() => window.getSelection(), [])
+    const [data, setData] = useState<Data>(null)
+    const [currentData, setCurrentData] = useState<Data>(null)
+    const [isCardUpdate, setIsCardUpdate] = useState(false)
+    const [isDataUpdate, setIsDataUpdate] = useState(false)
+    const [cursor, setCursor] = useState<Cursor>(null) // 가장 최근 커서
+    const [scroll, setScroll] = useState<Scroll>({x: 0, y: 0})
 
-    const {handleDrop, fromIndex, toIndex} = useCardDragDrop(cards, cardRefs.current, setStagedCardsFunc) // 카드 드래그&드랍 이동
+    // ------ 최신 데이터 가져오는 함수 ------
+    const getLatestCards = useCallback(() => {
+        if (!cards) return cards
+        return (cards.map((card) => ({
+            data: cardRefs.current[card.id]?.getData() ?? card.data,
+            id: card.id,
+            mode: card.mode,
+        })))
+    }, [cards])
+    const getLatestCursor = useCallback(() => {
+        if (!selection || selection.rangeCount === 0) return null
+        const range = selection.getRangeAt(0)
+        const {startContainer, startOffset, endContainer, endOffset} = range
+        return {
+            startContainer: startContainer,
+            startOffset: startOffset,
+            endContainer: endContainer,
+            endOffset: endOffset
+        }
+    }, [selection])
+    const getLatestScroll = useCallback((): Scroll => {
+        const {scrollX, scrollY} = window
+        return {
+            x: scrollX,
+            y: scrollY
+        }
+    }, [])
+    
+    // 매개변수 latestCards, latestCursor 를 받아 사용하는 공통 함수
+    const getDataCommonFunc = useCallback((latestCards: CardProps[], latestCursor: Cursor, latestScroll: Scroll): Data => {
+        if (currentData && isEqual(currentData.cards, latestCards)) { // 현 상태 history 와 카드가 같다면 커서와 스크롤은 이전 데이터 사용 즉, 업데이트 X
+            return currentData
+        }
+        return {
+            cards: latestCards,
+            cursor: latestCursor,
+            scroll: latestScroll
+        }
+    }, [currentData])
+    // 모든 최신 데이터 가져오기
+    const getLatestData = useCallback(():Data => getDataCommonFunc(getLatestCards(), getLatestCursor(), getLatestScroll()), [getDataCommonFunc, getLatestCards, getLatestCursor])
+    // 카드 업데이트시 데이터 업데이트
+    const getUpdateData = useCallback(():Data => getDataCommonFunc(cards, getLatestCursor(), getLatestScroll()), [getDataCommonFunc, cards, getLatestCursor])
 
-    // const [cursorStage, setCursorStage] = useState<(Cursor|null)[]>([])
-    // const [cursorHistory, setCursorHistory] = useState<(Cursor|null)[]>([])
-    //
-    // const getLatestCursor = useCallback((): Cursor|null => {
-    //     const selection = window.getSelection()
-    //
-    //     if (!selection || selection.rangeCount === 0) return null
-    //     const range = selection.getRangeAt(0)
-    //     const {startContainer, startOffset, endContainer, endOffset} = range
-    //     return {
-    //         startContainer: startContainer,
-    //         startOffset: startOffset,
-    //         endContainer: endContainer,
-    //         endOffset: endOffset
-    //     }
-    // }, [])
+    useEffect(() => { // 카드 변경으로 인한 상태 업데이트
+        if (!cards || cards.length === 0) return
+        if (isEqual(cards, data?.cards)) return
+        if (isDataUpdate) return
+        setIsCardUpdate(true)
+        setData(getUpdateData())
+    }, [cards]);
+    useEffect(() => { // undo|redo 상태 업데이트
+        if (!data) return;
+        if (isCardUpdate) return
+        setIsDataUpdate(true)
+        setCards(data.cards)
+    }, [data])
+
+    const {history, present, updateHistory} = useHistory<Data>(data, setData, getLatestData)
+
+    useEffect(() => { // 위에서 해당값을 사용하기 위함
+        setCurrentData(present)
+    }, [present]);
+    useEffect(() => { // 설정 초기화 setData 되기 전에 present 먼저 변경되므로 OK
+        setIsDataUpdate(false)
+        setIsCardUpdate(false)
+    }, [present]);
+
+    /* ========== 드래그 & 드랍 ========== */
+    const [fromIndex, setFromIndex] = useState(-1) // 드래그 할 위치
+    const [toIndex, setToIndex] = useState(-1) // 드랍할 위치
+
+    const handleDrop = useDrop({
+        dropTarget: cards && cards.length>0? cardRefs.current[cards[fromIndex]?.id]: null,
+        onDragStartBefore: (e?: MouseEvent<HTMLElement>) => { // 드래그 전 이미지 복사를 위한 타겟 설정
+            const index = parseInt(e?.currentTarget.dataset.targetIndex ?? '0')
+            setFromIndex(index)
+        },
+        onDragOver: (e?: MouseEvent<HTMLElement>) => { // 드래그 시 파란색 바 표시
+            let dropIndex = parseInt(e?.currentTarget.dataset.selectIndex ?? '-1')
+
+            if (dropIndex === fromIndex) { // 이동할 위치가 자기 자신 아래라면
+                dropIndex = -1
+            }
+            setToIndex(dropIndex)
+        },
+        onDragOut: () => { // 파란색 바 제거
+            setToIndex(-1)
+        },
+        onDrop: () => {
+            if (toIndex === -1) return // 이동 X
+            // const insertIndex = fromIndex <= toIndex ? toIndex : toIndex + 1; // 이동할 위치
+
+            setCards(() => {
+                const pre = getLatestCards()
+                if (toIndex === -1 || !pre) return pre // 이동 X
+                const insertIndex = fromIndex <= toIndex ? toIndex : toIndex + 1; // 이동할 위치
+
+                const copy = [...pre];
+                const [target] = copy.splice(fromIndex, 1) // splice: 삭제 요소 리턴
+                copy.splice(insertIndex, 0, target); // 시작 위치, 제거할 요소 개수, 추가할 요소들
+                return copy;
+            });
+        },
+    })
+
     // const setCursor = useCallback((cursor: Cursor|null) => {
     //     const range = document.createRange()
     //     const selection = window.getSelection()
@@ -129,103 +234,81 @@ const RichEditor = () => {
     //         selection?.addRange(range)
     //     }
     // }, [])
-    // const getLatestData = useCallback(() => {
-    //
-    // }, [])
-    const getLatestCards = useCallback(() => {
-        if (!cards) return cards
-        return (cards.map((card) => ({
-            data: cardRefs.current[card.id]?.getData() ?? card.data,
-            id: card.id,
-            mode: card.mode,
-        })))
-    }, [cards])
-
-    /* ========== 카드 현상태 업데이트 후 변경 ========== */
-    useEffect(() => { // STEP 1. 현 상태 업데이트
-        if (!stagedCardsFunc) return
-        setCards(getLatestCards())
-        setCanStageUpdate(true)
-    }, [stagedCardsFunc]);
-    useEffect(() => { // STEP 2. 새롭게 변경될 카드 업데이트 (이동, 삭제, 추가 등)
-        if (canStageUpdate && stagedCardsFunc) {
-            setCards(pre => stagedCardsFunc(pre))
-            setCanStageUpdate(false)
-        }
-    }, [canStageUpdate]);
 
     /* ========== History 설정 ========== */
-    const {present, updateHistory} = useHistory<CardProps[]>(cards, setCards, getLatestCards)
+    // useRichHistory(cards, setCards, cardRefs.current)
 
-    // history 업데이트 필요할 경우 감지
-    const [canUpdateHistory, setCanUpdateHistory] = useState<boolean>(false)
-
-    useLayoutEffect(() => { // history 업데이트
-        if (!canUpdateHistory) return
-        if (!isEqual(getLatestCards(), cards)) { // 신규 값이라면 History 업데이트
-            updateHistory(getLatestCards())
-        }
-
-        setCanUpdateHistory(false) // 값 원상복귀
-    }, [canUpdateHistory]);
-    useEffect(() => {
-        eventManager.addEventListener('focusin', 'RichEditor', (e) => { // // 타겟 포커스 변경시
-            if (!isCustomTextAreaElement(e.target as HTMLElement)) return // CustomTextAreaElement 타입이 아니라면
-            setCanUpdateHistory(true)
-        })
-        eventManager.addEventListener('keydown', 'RichEditor', (event) => { // 특정 키 history 업데이트
-            const e = event as KeyboardEvent
-            if (!isCustomTextAreaElement(e.target as HTMLElement)) return // CustomTextAreaElement 타입이 아니라면
-            if (e.key === 'Tab') {
-                setCanUpdateHistory(true)
-            }
-        })
-        return () => {
-            eventManager.removeEventListener('focusin', 'RichEditor')
-            eventManager.removeEventListener('keydown', 'RichEditor')
-        }
-    }, []);
+    // const {present, updateHistory} = useHistory<CardProps[]>(cards, setCards, getLatestCards)
     //
-    const [isEraseMode, setIsEraseMode] = useState<boolean|null>(null) // 텍스트 지우는 모드 진입과 쓰는 모드로 변경되는 타이밍 저장
-    const [prevUnicode, setPrevUnicode] = useState<number>(-1) // 한글 같은 조합형 문자의 추가와 삭제를 감지하기 위한 유니코드 저장 조합할 수록 유니코드는 커지며 음수는 없다
-    const [currentUnicode, setCurrentUnicode] = useState<number>(-1)
-
-    useLayoutEffect(() => { // 한글 조합 중
-        setPrevUnicode(currentUnicode) // 비동기적으로 동작해서 위로 올림
-        if (currentUnicode < 0) return // 조합 끝 또는 시작
-
-        if (currentUnicode < prevUnicode) setIsEraseMode(true) // 지우는 모드
-        else setIsEraseMode(false) // 쓰는 모드
-    }, [currentUnicode]);
-    useLayoutEffect(() => { // 텍스트 지우는 모드 True 와 False 로 변경되는 순간을 감지해 History 업데이트
-        if (isEraseMode === null) return
-        updateHistory(getLatestCards()) // 삭제 모드 진입과 지우는 모드로 전환되는 순간 감지 후 history 업데이트
-    }, [isEraseMode]);
-
-    useEffect(() => {
-        eventManager.addEventListener('beforeinput', 'RichEditor', (event) => { // 조합형 문자 조합 제외 삭제 모드 input 모드 감지
-            const e = event as InputEvent
-
-            if (e.inputType === 'deleteContentBackward' || e.inputType === 'deleteContentForward') {
-                setIsEraseMode(true)
-            } else if (e.inputType === 'insertText' || e.inputType === 'insertFromPaste' || e.inputType === 'insertFromDrop') {
-                setIsEraseMode(false)
-            } else if (e.isComposing) { // 조합형 문자 입력 중 상태 조합 중 삭제 포함
-                if (!e.data) return
-                const charCode = e.data.charCodeAt(0) // 빈문자열일 경우 NaN
-                if (isNaN(charCode)) setIsEraseMode(true) // 빈 문자열은 삭제 모드
-                setCurrentUnicode(isNaN(charCode) ? -1 : charCode)
-            }
-        })
-        eventManager.addEventListener('compositionend', 'RichEditor', () => { // 한 글자 완성시 유니 코드 초기화
-            setCurrentUnicode(-1)
-            setPrevUnicode(-1)
-        })
-        return () => {
-            eventManager.removeEventListener('beforeinput', 'RichEditor')
-            eventManager.removeEventListener('compositionend', 'RichEditor')
-        }
-    }, []);
+    // // history 업데이트 필요할 경우 감지
+    // const [canUpdateHistory, setCanUpdateHistory] = useState<boolean>(false)
+    //
+    // useLayoutEffect(() => { // history 업데이트
+    //     if (!canUpdateHistory) return
+    //     if (!isEqual(getLatestCards(), cards)) { // 신규 값이라면 History 업데이트
+    //         updateHistory(getLatestCards())
+    //     }
+    //
+    //     setCanUpdateHistory(false) // 값 원상복귀
+    // }, [canUpdateHistory]);
+    // useEffect(() => {
+    //     eventManager.addEventListener('focusin', 'RichEditor', (e) => { // // 타겟 포커스 변경시
+    //         if (!isCustomTextAreaElement(e.target as HTMLElement)) return // CustomTextAreaElement 타입이 아니라면
+    //         setCanUpdateHistory(true)
+    //     })
+    //     eventManager.addEventListener('keydown', 'RichEditor', (event) => { // 특정 키 history 업데이트
+    //         const e = event as KeyboardEvent
+    //         if (!isCustomTextAreaElement(e.target as HTMLElement)) return // CustomTextAreaElement 타입이 아니라면
+    //         if (e.key === 'Tab') {
+    //             setCanUpdateHistory(true)
+    //         }
+    //     })
+    //     return () => {
+    //         eventManager.removeEventListener('focusin', 'RichEditor')
+    //         eventManager.removeEventListener('keydown', 'RichEditor')
+    //     }
+    // }, []);
+    // //
+    // const [isEraseMode, setIsEraseMode] = useState<boolean|null>(null) // 텍스트 지우는 모드 진입과 쓰는 모드로 변경되는 타이밍 저장
+    // const [prevUnicode, setPrevUnicode] = useState<number>(-1) // 한글 같은 조합형 문자의 추가와 삭제를 감지하기 위한 유니코드 저장 조합할 수록 유니코드는 커지며 음수는 없다
+    // const [currentUnicode, setCurrentUnicode] = useState<number>(-1)
+    //
+    // useLayoutEffect(() => { // 한글 조합 중
+    //     setPrevUnicode(currentUnicode) // 비동기적으로 동작해서 위로 올림
+    //     if (currentUnicode < 0) return // 조합 끝 또는 시작
+    //
+    //     if (currentUnicode < prevUnicode) setIsEraseMode(true) // 지우는 모드
+    //     else setIsEraseMode(false) // 쓰는 모드
+    // }, [currentUnicode]);
+    // useLayoutEffect(() => { // 텍스트 지우는 모드 True 와 False 로 변경되는 순간을 감지해 History 업데이트
+    //     if (isEraseMode === null) return
+    //     updateHistory(getLatestCards()) // 삭제 모드 진입과 지우는 모드로 전환되는 순간 감지 후 history 업데이트
+    // }, [isEraseMode]);
+    //
+    // useEffect(() => {
+    //     eventManager.addEventListener('beforeinput', 'RichEditor', (event) => { // 조합형 문자 조합 제외 삭제 모드 input 모드 감지
+    //         const e = event as InputEvent
+    //
+    //         if (e.inputType === 'deleteContentBackward' || e.inputType === 'deleteContentForward') {
+    //             setIsEraseMode(true)
+    //         } else if (e.inputType === 'insertText' || e.inputType === 'insertFromPaste' || e.inputType === 'insertFromDrop') {
+    //             setIsEraseMode(false)
+    //         } else if (e.isComposing) { // 조합형 문자 입력 중 상태 조합 중 삭제 포함
+    //             if (!e.data) return
+    //             const charCode = e.data.charCodeAt(0) // 빈문자열일 경우 NaN
+    //             if (isNaN(charCode)) setIsEraseMode(true) // 빈 문자열은 삭제 모드
+    //             setCurrentUnicode(isNaN(charCode) ? -1 : charCode)
+    //         }
+    //     })
+    //     eventManager.addEventListener('compositionend', 'RichEditor', () => { // 한 글자 완성시 유니 코드 초기화
+    //         setCurrentUnicode(-1)
+    //         setPrevUnicode(-1)
+    //     })
+    //     return () => {
+    //         eventManager.removeEventListener('beforeinput', 'RichEditor')
+    //         eventManager.removeEventListener('compositionend', 'RichEditor')
+    //     }
+    // }, []);
 
     //     eventManager.addEventListener('input', 'RichEditor', (event) => {
     //         const e = event as InputEvent
@@ -286,6 +369,7 @@ const RichEditor = () => {
     // }, [historyIndex]);
 
 
+    /* ========== 이벤트 핸들러 정의 ========== */
     const handleAddCard = (index: number) => ({
         onClick: () => {
             // const addIndex = index + 1
@@ -298,10 +382,13 @@ const RichEditor = () => {
         }
     })
 
-    const handleCard = () => ({
-        onBlur: () => {
+    const handleCard = {
+        onBlur: () => { // 임시
+            updateHistory(getLatestData())
+            // setCursor(null)
+            // setScroll(pre=>pre)
         }
-    })
+    }
 
     return (<DragDropProvider useDrop={handleDrop}><Container>
         {cards.map((card, index) => {
@@ -334,7 +421,8 @@ const RichEditor = () => {
                         } else { // 언마운트시 실행된다는데 인 필요
                             delete cardRefs.current[card.id]
                         }
-                    }} mode={card.mode} data={card.data} /></Card>
+                    }} mode={card.mode} data={card.data} {...handleCard}
+                    /></Card>
                 </DraggableCard>
                 {/* 카드 나누는 기준 + 카드 추가 */}
                 <CardDivider>
