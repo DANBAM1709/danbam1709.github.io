c3e42fa551f09e0347cedcc3166b94f88e329e04
DANBAM <danbam1709@gmail.com>
2025-03-01 13:33:30 +0900

    useHistory.ts undo, redo 이벤트 밖에서 사용가능하도록 변경

--- src/common/history/useHistory.ts
+++ src/common/history/useHistory.ts
@@ -1,13 +1,12 @@
 import {Dispatch, SetStateAction, useCallback, useEffect, useState} from "react";
 import isEqual from "fast-deep-equal";
-import {eventManager} from "../../global/event.ts";
 
 /*
 useUndoRedo<T> 명시할 것 <br />
 data: 저장할 데이터 변경됨을 인식하고 history 에 저장됨 <br />
 setData: 데이터를 저장할 것을 가져오기 <br />
 getLatestData: () => T[] 최근 데이터 가져오기(데이터만!) <br />
-return {present: 선택된 현 상태 data, updateHistory: (T)=>void 상태 변경 없이 history 만 업데이트 하는 함수}
+return {present: 선택된 현 상태 data, updateHistory: (T, isReplaceMode?<boolean>)=>void 상태 변경 없이 history 만 업데이트 하는 함수 isReplaceMode=true 설정시 이전 상태와 바꿀 수 있다}
  */
 const useHistory = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatestData: ()=>T) => {
     const [isUndo, setIsUndo] = useState<boolean>(false) // 뒤로
@@ -19,12 +18,12 @@ const useHistory = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatestD
     const [isLatestIndex, setIsLatestIndex] = useState<boolean>(true)
     const [present, setPresent] = useState<T|null>(null)
 
-    const [hasLatestDataUpdate, setHasLatestDataUpdate] = useState<boolean>(false) // 뒤로 가기를 하면서 최신 데이터를 history 에 업로드 할 경우 setData의 값이 변경되지 않기 때문에 일어나지 않는 리랜더링 문제 해결을 위함  
+    const [hasLatestDataUpdate, setHasLatestDataUpdate] = useState<boolean>(false) // 뒤로 가기를 하면서 최신 데이터를 history 에 업로드 할 경우 setData의 값이 변경되지 않기 때문에 일어나지 않는 리랜더링 문제 해결을 위함
 
     /* =============== 업데이트 함수 정의 =============== */
     const updateHistory = useCallback((updateData: T) => { // 업데이트 함수
         setHistory(prev => {
-            const copy = isLatestIndex? [...prev]: prev.slice(0, index+1)
+            const copy = prev.slice(0, index+1)
             setIsLatestIndex(true) // 현재 마지막 인덱스 선택
 
             const lastRecord = copy[copy.length-1]
@@ -54,11 +53,11 @@ const useHistory = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatestD
     }, [data])
 
     useEffect(() => { // 마지막 인덱스 선택
-        if (isUndo || isRedo || history.length <= 0) return
+        if (isUndo || isRedo || history.length === 0) return
         setIndex(history.length-1)
     }, [history]);
 
-    useEffect(() => { // STEP 2 새로운 데이터로 업로드 step1은 아래에
+    useEffect(() => { // step2 새로운 데이터로 업로드 step1은 아래에
         if (!hasLatestDataUpdate) return
         const selectedIndex = (history.length-2>=0)? history.length-2: 0
         setCanHistoryUpdate(false) // history 업로드 방지
@@ -68,15 +67,13 @@ const useHistory = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatestD
     }, [hasLatestDataUpdate]);
 
     // history 업데이트 후 undo, redo 상태 업데이트
-    useEffect(() => {
+    useEffect(() => { // STEP3
         if (isUndo) {
             const selectedIndex = (history.length-2>=0)? history.length-2: 0
-            setIndex(prevIndex => {
-                if (prevIndex === selectedIndex) {
-                    setCanHistoryUpdate(false) // history 업로드 방지
-                    setData(history[selectedIndex+1]) // STEP 1 리랜더링을 위한 최신 데이터 업로드 step2 는 위
-                    setHasLatestDataUpdate(true) // STEP 2로 보내기
-                }
+            setIndex(() => {
+                setCanHistoryUpdate(false) // history 업로드 방지
+                setData(getLatestData()) // 리랜더링을 위한 최신 데이터 업로드 step2 는 위
+                setHasLatestDataUpdate(true) // step2로 보내기
                 return selectedIndex
             })
             setIsUndo(false)
@@ -89,19 +86,22 @@ const useHistory = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatestD
     }, [history]);
 
     // undo redo 명령받은 직후 history 업데이트 여부 확인
-    useEffect(() => {
-        if (isUndo || isRedo) {
+    useEffect(() => { // STEP1
+        if (isUndo) {
             const latestData = getLatestData()
             if (isEqual(latestData, history[index])) { // 업데이트 할 값과 현재 값 비교
-                setIsUndoRedo(true) // 아래로
+                setIsUndoRedo(true) // 아래로 STEP2로
             } else {
-                updateHistory(latestData) // history 업데이트 | 위로
+                updateHistory(latestData) // history 업데이트 | 위로 업데이트 후 STEP3 으로 이동
             }
+        }
+        if (isRedo) {
+            setIsUndoRedo(true) // 아래로
         }
     }, [isUndo, isRedo]);
 
     // history 업데이트 없이 undo, redo 상태 업데이트 인덱스로 인덱스 변경으로 이동
-    useEffect(() => {
+    useEffect(() => { // STEP2
         if (!isUndoRedo) return
 
         setIsLatestIndex(false) // 아래 index 변경으로 인한 것을 실행시키기 위한 임시 값 어차피 그 아래에 true 가능하게 해뒀음
@@ -137,25 +137,11 @@ const useHistory = <T,>(data:T, setData: Dispatch<SetStateAction<T>>, getLatestD
     }, [index]);
 
     /* =============== 이벤트 리스너 =============== */
-    useEffect(() => {
-        eventManager.addEventListener('keydown', 'useUndoRedo', (event) => {
-            const e = event as KeyboardEvent
-            if (e.ctrlKey && e.key.toLowerCase() === 'z' ) {
-                e.preventDefault() // 브라우저 기본 뒤로 가기 방지
-                setIsUndo(true)
-            }
-            if (e.ctrlKey && e.key.toLowerCase() === 'y' ) {
-                e.preventDefault() // 브라우저 기본 앞으로 가기 방지
-                setIsRedo(true)
-            }
-        })
+    const undo = useCallback(() => setIsUndo(true), [])
+    const redo = useCallback(() => setIsRedo(true), [])
 
-        return () => {
-            eventManager.removeEventListener('keydown', 'useUndoRedo')
-        }
-    }, []);
 
-    return {history, present, updateHistory}
+    return {present, updateHistory, undo, redo}
 }
 
 export default useHistory
