be49fd782c526979c8fa037c1c67389fcd02a26e
DANBAM <danbam1709@gmail.com>
2025-03-03 18:25:56 +0900

    useCursorManager.ts moveCursor 동작이 끝나지 않은 상태로 다른 위치 선택시 커서가 되돌아가는 현상 방지

--- src/common/hook/useCursorManager.ts
+++ src/common/hook/useCursorManager.ts
@@ -1,4 +1,5 @@
-import {useCallback} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
+import {eventManager} from "../../global/event.ts";
 
 interface  SearchOffsetState {
     pos: number;
@@ -14,6 +15,40 @@ interface SearchNodesState {
 
 // return getCursorOffsets(currentEditable: HTMLElement), moveCursor(currentEditable, startPos, endPos)
 const useCursorManager = () => {
+    const [isSelection, setIsSelection] = useState<boolean>(false) // 선택 영역 감지 0 은 없을 때
+    const isComposingRef = useRef<boolean>(false) // composing 일 땐 선택 영역으로 값이 들어갈 수 있으므로
+
+    useEffect(() => {
+        const detail = {selection: false} // 선택 영역 감지
+        const event = new CustomEvent('customCursorEvent', {detail: detail})
+
+        eventManager.addEventListener('selectionchange', 'useCursorManager', () => {
+            const selection = window.getSelection()
+            if (selection?.isCollapsed) { // 없음
+                setIsSelection(false)
+                if (isSelection) { // 커서 없어지는 것도 감지 필요할 듯?}
+                    detail.selection = false
+                    document.dispatchEvent(event)
+                }
+            } else { // 선택 영역에 변동이 있을 때
+                setIsSelection(true)
+                detail.selection = true
+                document.dispatchEvent(event) // 선택 영역 변경 시 감지
+            }
+        })
+
+        return () => eventManager.removeEventListener('selectionchange', 'useCursorManager')
+    }, [isSelection]);
+
+    useEffect(() => {
+        eventManager.addEventListener('compositionstart', 'useCursorManager', () => {isComposingRef.current = true})
+        eventManager.addEventListener('compositionend', 'useCursorManager', () => {isComposingRef.current = false})
+
+        return () => {
+            eventManager.removeEventListener('compositionstart', 'useCursorManager')
+            eventManager.removeEventListener('compositionend', 'useCursorManager')
+        }
+    }, []);
 
     // ============== 커서 위치 찾기 ==============
     // 노드 순회하면서 위치 찾기 (재귀)
@@ -57,14 +92,13 @@ const useCursorManager = () => {
 
         const hasElementNode = Array.from(element.childNodes).some(node => node.nodeType === Node.ELEMENT_NODE);
 
+        let startPos = 0, endPos = 0
+
         // element = startContainer = endContainer, 텍스트 노드뿐
         if (element === startContainer && !hasElementNode) {
-            return { startPos: startOffset, endPos: endOffset };
-        }
-
-        let startPos = 0, endPos = 0
-        // element = startContainer = endContainer, 텍스트 노드가 아닌게 있다면?
-        if (element === startContainer && hasElementNode) {
+            startPos = startOffset
+            endPos = endOffset
+        } else if (element === startContainer && hasElementNode) { // element = startContainer = endContainer, 텍스트 노드가 아닌게 있다면?
             for (let i = 0; i <= endOffset; i++) {
                 const child = element.childNodes[i]
                 const length = child.textContent?.length ?? 0
@@ -73,30 +107,62 @@ const useCursorManager = () => {
                 }
                 endPos += length
             }
-            return { startPos: startPos, endPos: endPos };
+        } else {
+            const state = {
+                pos: 0, // 지금까지 찾은 위치
+                index: 0, // endContainer 를 조회하기 시작할 위치 즉, startContainer 찾은 인덱스
+                previousPos: 0 // 여기서 startContainer 를 찾기 전까지의 길이
+            }
+
+            // 시작 위치 찾기
+            const nodes = [...element.childNodes]
+            searchPos(nodes, state, startContainer, startOffset, true)
+            startPos = state.pos
+
+            // 시작 위치부터 시작!
+            state.pos = state.previousPos
+            const remainingNodes = nodes.slice(state.index)
+            searchPos(remainingNodes, state, endContainer, endOffset, false)
+            endPos = state.pos
         }
 
-        const state = {
-            pos: 0, // 지금까지 찾은 위치
-            index: 0, // endContainer 를 조회하기 시작할 위치 즉, startContainer 찾은 인덱스
-            previousPos: 0 // 여기서 startContainer 를 찾기 전까지의 길이
-        }
-
-        // 시작 위치 찾기
-        const nodes = [...element.childNodes]
-        searchPos(nodes, state, startContainer, startOffset, true)
-        startPos = state.pos
-
-        // 시작 위치부터 시작!
-        state.pos = state.previousPos
-        const remainingNodes = nodes.slice(state.index)
-        searchPos(remainingNodes, state, endContainer, endOffset, false)
-        endPos = state.pos
-
-        return { startPos: startPos, endPos: endPos };
+        // 선택영역이 없다면 그리고 글자 조합 중이라면
+        if (selection.isCollapsed || isComposingRef.current) return {startPos: endPos, endPos: endPos}
+        return { startPos: startPos, endPos: endPos }; // 선택영역이 있다면
     }, [searchPos]);
 
     // ============== 커서 위치 세팅 ==============
+    const [canMove, setCanMove] = useState(true) // 기본 이동 가능 상태
+    const [movePosition, setMovePosition] = useState<{startContainer: Node, startOffset: number, endContainer: Node, endOffset: number}|null>(null)
+
+    useEffect(() => {
+        // 방향키는 어쩔까... 일단 남겨놓을까? 거 괜히 불편해질 수 있을거 같은디 흠..
+        const handleBlur = () => setCanMove(false)
+        const handleClick = () => setCanMove(false)
+        document.addEventListener('click', handleClick, true) // 클릭시 이동 방지
+        document.addEventListener('blur', handleBlur, true) // 캡처링 단계에서 모든 blur 이벤트를 감지
+
+        return () => {
+            document.removeEventListener('click', handleClick, true)
+            document.removeEventListener('blur', handleBlur, true)
+        }
+    }, []);
+    useEffect(() => { // 위치 계산하는 동안 클릭등의 이벤트가 들어왔다면 커서 이동 취소하기 그 사이 입력도 막아야 하나 아직 고민임 써보고 불편하면 ㅇㅇ
+        if (!movePosition || !canMove) return
+        const range = document.createRange();
+        const selection = window.getSelection();
+        const {startContainer, startOffset, endContainer, endOffset} = movePosition
+
+        if (startContainer && endContainer) {
+            range.setStart(startContainer, startOffset)
+            range.setEnd(endContainer, endOffset)
+
+            if (selection) {
+                selection.removeAllRanges();
+                selection.addRange(range);
+            }
+        }
+    }, [movePosition]);
     // 노드를 순회하며 노드 찾기 및 offset 찾기 (재귀)
     const searchNodes = useCallback((nodes: Node[], state: SearchNodesState, prevLength: number, searchPos: number, rootCheck: boolean) => {
         let isContain = false
@@ -134,9 +200,7 @@ const useCursorManager = () => {
     }, [])
     const moveCursor = useCallback((element: HTMLElement|null, startPos: number, endPos: number) => {
         if (!element) return;
-
-        const range = document.createRange();
-        const selection = window.getSelection();
+        setCanMove(true)
 
         const state = {
             node: null, // 찾은 노드
@@ -158,13 +222,12 @@ const useCursorManager = () => {
         const endOffset = state.offset
 
         if (startContainer && endContainer) {
-            range.setStart(startContainer, startOffset)
-            range.setEnd(endContainer, endOffset)
-
-            if (selection) {
-                selection.removeAllRanges();
-                selection.addRange(range);
-            }
+            setMovePosition({
+                startContainer: startContainer,
+                startOffset: startOffset,
+                endContainer: endContainer,
+                endOffset: endOffset
+            })
         }
     }, [searchNodes])
 
