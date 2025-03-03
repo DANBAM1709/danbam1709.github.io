a851efc4ec016837e1db28a5999ef6d26b4fcdd1
DANBAM <danbam1709@gmail.com>
2025-03-03 19:54:18 +0900

    LayoutContext.ts 레이아웃과 관련된 Reducer Provider 생성 목적인데 없앨 가능성이 높을듯?

---
+++ src/common/contexts/LayoutContext.ts
@@ -0,0 +1,12 @@
+import createReducerContext from "./createReducerContext.tsx";
+import {
+    initRichEditorState,
+    RichEditorAction,
+    richEditorReducer,
+    RichEditorState
+} from "../../features/editor/richEditor.reducer.ts";
+
+// 리치 에디터 reducer Provider
+const {Provider: createRichEditorProvider, useGenericContext: useGenericRichEditorContext} = createReducerContext<RichEditorState, RichEditorAction>(richEditorReducer, initRichEditorState)
+export const RichEditorProvider = createRichEditorProvider
+export const useRichEditorContext = useGenericRichEditorContext
