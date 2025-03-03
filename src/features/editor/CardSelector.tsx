e4b163a575a12faa5eebd089f9ce7410b210476c
DANBAM <danbam1709@gmail.com>
2025-03-03 21:26:37 +0900

    CardSelector.tsx Component 선택 모드 변경

--- src/features/editor/CardSelector.tsx
+++ src/features/editor/CardSelector.tsx
@@ -1,33 +1,27 @@
-import {forwardRef, ReactElement, useEffect, useState} from "react";
+import {ComponentPropsWithoutRef, forwardRef} from "react";
 import BasicCard from "./cards/BasicCard.tsx";
 import TitleCard from "./cards/TitleCard.tsx";
-import {GetDataHTMLElement} from "../../layout/RichEditor.tsx";
+import {GetDataHTMLElement} from "./RichEditor.tsx";
 
 export type CardProps =
-    {id: string, mode: 'title', data: string} | // data: content
-    {id: string, mode: 'default', data: string} | // data: content
-    {id: string, mode: 'code', data: string}  // data: content
+    {id: string, mode: 'title', data: {html: string}} | // data: content
+    {id: string, mode: 'default', data: {html: string}} | // data: content
+    {id: string, mode: 'code', data: {html: string}}  // data: content
 
+type CardSelectorProps = Omit<CardProps, 'id'> & ComponentPropsWithoutRef<'div'>
+export type InheritCardProps = Omit<CardProps, 'id'|'mode'> & ComponentPropsWithoutRef<'div'>
 
-const CardSelector = forwardRef<GetDataHTMLElement, Omit<CardProps, 'id'>>(({mode, data}, ref) => {
-    const [component, setComponent] = useState<ReactElement>(<></>)
-
-    useEffect(() => {
-        setComponent(() => {
-            switch (mode) {
-                case 'title':
-                    return <TitleCard ref={ref} data-placeholder={'제목'}>{data}</TitleCard>
-                case 'default':
-                    return <BasicCard ref={ref} html={data} />
-                // case 'code':
-                //     return <CodeStyle id={id} ref={ref}>{data}</CodeStyle>
-                default:
-                    return <></>
-            }
-        })
-    }, [mode, data, ref]);
-
-    return (<>{component}</>)
+const CardSelector = forwardRef<GetDataHTMLElement, CardSelectorProps>(({mode, data, ...rest}, ref) => {
+    switch (mode) {
+        case 'title':
+            return <TitleCard ref={ref} data-placeholder={'제목'} data={data} {...rest} />
+        case 'default':
+            return <BasicCard ref={ref} data={data} {...rest} />
+        // case 'code':
+        //     return <CodeStyle id={id} ref={ref}>{data}</CodeStyle>
+        default:
+            return <></>
+    }
 })
 
 export default CardSelector
