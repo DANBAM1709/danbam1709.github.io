18ef2cbe6b39399ba75d1ff242f73fa444b95133
DANBAM <danbam1709@gmail.com>
2025-03-03 23:10:46 +0900

    테스트를 위해 전부 주석처리

--- src/features/editor/hook/useCardDelete.ts
+++ src/features/editor/hook/useCardDelete.ts
@@ -1,44 +1,45 @@
-import {Dispatch, SetStateAction, useEffect} from "react";
-import {CardProps} from "../CardSelector.tsx";
-import {GetDataHTMLElement} from "../RichEditor.tsx";
-
-const useCardDelete = (deleteEl: HTMLElement|null, setCards: Dispatch<SetStateAction<CardProps[]>>, cardRefs: (GetDataHTMLElement|null)[]) => {
-
-    useEffect(() => {
-        if(!deleteEl) return
-        setCards(cards => {
-            if (cards.length === 2) { // 제목과 기본 카드라면 삭제 막기
-                const defaultCard = cards[1]
-                const isDefaultEmpty = defaultCard.mode === 'default' && defaultCard.data === ''
-
-                if (!isDefaultEmpty) { // 첫 줄 기본 카드 검증 실패시 추가 데이터
-                    return [cards[0], {id: crypto.randomUUID(), mode: 'default', data: ''}]
-                }
-            }
-
-            const deleteIndex = cardRefs.findIndex(ref => ref === deleteEl) // 삭제 인덱스 확인
-            if (deleteIndex > 0) return cards.filter((_, i) => i !== deleteIndex) // 삭제할 요소가 있다면
-            return cards
-        })
-
-        // // 카드 삭제 후 이전 카드 선택.. 인데 이걸 나중에 바꿀거임
-        // const deleteIndex = cardRefs.current.findIndex(ref => ref === deleteEl);
-        // if (deleteIndex > 0) {
-        //     // 이전 노드 선택
-        //     const node = cardRefs.current[deleteIndex-1]!
-        //     const selection = window.getSelection()
-        //     if (selection) {
-        //         const range = document.createRange()
-        //         range.selectNodeContents(node)
-        //         range.collapse(false) // 마지막으로 이동
-        //
-        //         selection.removeAllRanges()
-        //         selection.addRange(range)
-        //     }
-        // }
-    }, [deleteEl]);
-
-    return {}
-}
-
-export default useCardDelete 
+// import {Dispatch, SetStateAction, useEffect} from "react";
+// import {CardProps} from "../CardSelector.tsx";
+// import {GetDataHTMLElement} from "../RichEditor.tsx";
+//
+// const useCardDelete = (deleteEl: HTMLElement|null, setCards: Dispatch<SetStateAction<CardProps[]>>, cardRefs: (GetDataHTMLElement|null)[]) => {
+//
+//
+//     // useEffect(() => {
+//     //     if(!deleteEl) return
+//     //     setCards(cards => {
+//     //         if (cards.length === 2) { // 제목과 기본 카드라면 삭제 막기
+//     //             const defaultCard = cards[1]
+//     //             const isDefaultEmpty = defaultCard.mode === 'default' && defaultCard.data === ''
+//     //
+//     //             if (!isDefaultEmpty) { // 첫 줄 기본 카드 검증 실패시 추가 데이터
+//     //                 return [cards[0], {id: crypto.randomUUID(), mode: 'default', data: ''}]
+//     //             }
+//     //         }
+//     //
+//     //         const deleteIndex = cardRefs.findIndex(ref => ref === deleteEl) // 삭제 인덱스 확인
+//     //         if (deleteIndex > 0) return cards.filter((_, i) => i !== deleteIndex) // 삭제할 요소가 있다면
+//     //         return cards
+//     //     })
+//
+//         // // 카드 삭제 후 이전 카드 선택.. 인데 이걸 나중에 바꿀거임
+//         // const deleteIndex = cardRefs.current.findIndex(ref => ref === deleteEl);
+//         // if (deleteIndex > 0) {
+//         //     // 이전 노드 선택
+//         //     const node = cardRefs.current[deleteIndex-1]!
+//         //     const selection = window.getSelection()
+//         //     if (selection) {
+//         //         const range = document.createRange()
+//         //         range.selectNodeContents(node)
+//         //         range.collapse(false) // 마지막으로 이동
+//         //
+//         //         selection.removeAllRanges()
+//         //         selection.addRange(range)
+//         //     }
+//         // }
+//     // }, [deleteEl]);
+//
+//     return {}
+// }
+//
+// export default useCardDelete
--- src/layout/NavSidebar.tsx
+++ src/layout/NavSidebar.tsx
@@ -1,33 +1,33 @@
-import styled from "styled-components";
-import MenuBtn from "../uncategorized/test/MenuBtn.tsx";
-
-const Container = styled.div`
-    height: 100vh;
-    max-height: 100vh;
-    width: var(--sidebar-width);
-    
-    & > div {
-        position: fixed;
-        height: 100vh;
-        max-height: 100vh;
-        width: var(--sidebar-width);
-        box-shadow: rgba(0, 0, 0, 0.024) -1px 0 0 0 inset;
-        background: rgb(248, 248, 247);
-        z-index: 10;
-    }
-`
-
-const NavSidebar = () => {
-    return (<Container><div style={{position: 'fixed'}}>
-        <MenuBtn>
-            <img src={'search.svg'} alt={'search.svg'} width={'20px'} height={'20px'} />
-            <span>검색</span>
-        </MenuBtn>
-        <MenuBtn $depth={1}>
-            <img src={'search.svg'} alt={'search.svg'} width={'20px'} height={'20px'} />
-            <span>검색</span>
-        </MenuBtn>
-    </div></Container>)
-}
-
-export default NavSidebar
+// import styled from "styled-components";
+// import MenuBtn from "../uncategorized/test/MenuBtn.tsx";
+//
+// const Container = styled.div`
+//     height: 100vh;
+//     max-height: 100vh;
+//     width: var(--sidebar-width);
+//
+//     & > div {
+//         position: fixed;
+//         height: 100vh;
+//         max-height: 100vh;
+//         width: var(--sidebar-width);
+//         box-shadow: rgba(0, 0, 0, 0.024) -1px 0 0 0 inset;
+//         background: rgb(248, 248, 247);
+//         z-index: 10;
+//     }
+// `
+//
+// const NavSidebar = () => {
+//     return (<Container><div style={{position: 'fixed'}}>
+//         <MenuBtn>
+//             <img src={'search.svg'} alt={'search.svg'} width={'20px'} height={'20px'} />
+//             <span>검색</span>
+//         </MenuBtn>
+//         <MenuBtn $depth={1}>
+//             <img src={'search.svg'} alt={'search.svg'} width={'20px'} height={'20px'} />
+//             <span>검색</span>
+//         </MenuBtn>
+//     </div></Container>)
+// }
+//
+// export default NavSidebar
