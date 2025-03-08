c15435ecd665b925203acd0da573cab10f41d08e
DANBAM <danbam1709@gmail.com>
2025-03-08 21:06:23 +0900

    MainContainer.tsx 스크롤 잘리는 현상 해결

--- src/base-style/MainContainer.tsx
+++ src/base-style/MainContainer.tsx
@@ -3,11 +3,8 @@ import styled from "styled-components";
 const MainContainer = styled.div.attrs({ tabIndex: 0 })`
     position: relative;
     min-height: 100vh;
-    display: flex;
     align-items: center;
     flex-direction: column;
-    box-sizing: border-box;
-    width: 100%;
 `
 
 export default MainContainer
