2483b20d002c80d9df3c88864b308222dcc16474
DANBAM <danbam1709@gmail.com>
2025-03-08 11:29:28 +0900

    RichEditorProvider.tsx Card -> Content

--- src/features/editor/RichEditorProvider.tsx
+++ src/features/editor/RichEditorProvider.tsx
@@ -1,11 +1,11 @@
 import {ReactNode} from "react";
-import CardStoreProvider from "./provider/CardStoreProvider.tsx";
-import CardHistoryProvider from "./provider/CardHistoryProvider.tsx";
+import ContentStoreProvider from "./provider/ContentStoreProvider.tsx";
+import ContentHistoryProvider from "./provider/ContentHistoryProvider.tsx";
 
 const RichEditorProvider = ({children}: {children: ReactNode}) => {
-    return (<CardStoreProvider><CardHistoryProvider>
+    return (<ContentStoreProvider><ContentHistoryProvider>
         {children}
-    </CardHistoryProvider></CardStoreProvider>)
+    </ContentHistoryProvider></ContentStoreProvider>)
 }
 
 export default RichEditorProvider
