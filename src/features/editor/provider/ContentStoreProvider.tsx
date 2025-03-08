1c17150c4ca2b97920f8a63aa65354be0190cc53
DANBAM <danbam1709@gmail.com>
2025-03-08 11:31:29 +0900

    ContentStoreProvider.tsx 데이터를 가져오는 함수 이곳으로 이동

--- src/features/editor/provider/ContentStoreProvider.tsx
+++ src/features/editor/provider/ContentStoreProvider.tsx
@@ -1,12 +1,14 @@
-import ContentStoreContext, {ContentElement} from "./ContentStoreContext.ts";
-import {ReactNode, useEffect, useMemo, useRef, useState} from "react";
+import ContentStoreContext, {ContentElement, Cursor, Scroll} from "../context/ContentStoreContext.ts";
+import {ReactNode, useCallback, useEffect, useMemo, useRef, useState} from "react";
 import {ContentProps} from "../ContentSelector.tsx";
+import useCursorManager from "../../../hook/useCursorManager.ts";
 
 
 const ContentStoreProvider = ({children}: {children: ReactNode}) => {
     const [contents, setContents] = useState<ContentProps[]>([])
     const contentRefs = useRef<{ [id: string]: ContentElement | null }>({}); // 객체를 card.id로 관리
 
+    const {getCursorIndices} = useCursorManager()
     useEffect(() => {
         setContents([
             {data: {html: '1'}, id: crypto.randomUUID(), mode: 'basic'},
@@ -15,8 +17,28 @@ const ContentStoreProvider = ({children}: {children: ReactNode}) => {
             {data: {html: '4'}, id: crypto.randomUUID(), mode: 'basic'},
         ])
     }, []);
+    // ========= 최신 데이터 가져오는 함수 =========
+    const getLatestCursor = useCallback((element: HTMLElement|null):Cursor|null => {
+        if (!element) return null
+        const cursorPos = getCursorIndices(element)
+        if (!cursorPos) throw new Error('ContentHistoryProvider: cursorPos is null')
 
-    const value = useMemo(() => ({contents, setContents, contentRefs}), [contents])
+        const {startIndex, endIndex} = cursorPos
+        return {startIndex: startIndex, endIndex: endIndex, element: element};
+    }, [getCursorIndices])
+    const getLatestScroll = useCallback((): Scroll => {
+        return {x: window.scrollX, y: window.scrollY};
+    }, [])
+    const getLatestContents = useCallback(():ContentProps[] => { // 최신 카드 데이터
+        if (!contents) throw new Error('ContentHistoryProvider: A problem occurred while fetching the latest cards')
+        return (contents.map((content) => ({
+            data: contentRefs.current[content.id]?.getData() ?? content.data,
+            id: content.id,
+            mode: content.mode,
+        })));
+    }, [contents, contentRefs]);
+
+    const value = useMemo(() => ({contents, contentRefs, setContents, getLatestScroll, getLatestCursor, getLatestContents}), [contents, getLatestContents, getLatestCursor, getLatestScroll])
     
     return (<ContentStoreContext.Provider value={value}>{children}</ContentStoreContext.Provider>)
 }
