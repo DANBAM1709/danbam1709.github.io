d7885311313438ecb551c1d503fc1f4b8bba6d2c
DANBAM <danbam1709@gmail.com>
2025-03-08 11:31:38 +0900

    ContentStoreProvider.tsx 데이터를 가져오는 함수 이곳으로 이동

--- src/features/editor/context/ContentStoreContext.ts
+++ src/features/editor/context/ContentStoreContext.ts
@@ -1,6 +1,12 @@
 import {createContext, Dispatch, MutableRefObject, SetStateAction} from "react";
 import {ContentProps} from "../ContentSelector.tsx";
+import {CursorIndices} from "../../../hook/useCursorManager.ts";
 
+export type Cursor = CursorIndices & {element: HTMLElement|null} | null
+export type Scroll = {
+    x: number
+    y: number
+}
 
 // 자식 컴포넌트에서 노출할 ref 타입
 export interface ContentElement extends HTMLElement {
@@ -11,12 +17,18 @@ interface ContentStoreContextType {
     contentRefs: MutableRefObject<{ [id: string]: ContentElement | null }>
     contents: ContentProps[]
     setContents: Dispatch<SetStateAction<ContentProps[]>>
+    getLatestScroll: () => Scroll
+    getLatestCursor: (element: HTMLElement|null) => Cursor|null
+    getLatestContents: () => ContentProps[]
 }
 
 const ContentStoreContext = createContext<ContentStoreContextType>({
     contentRefs: {current: {}},
     contents: [],
     setContents: () => {},
+    getLatestScroll: () => ({x: 0, y: 0} as Scroll),
+    getLatestCursor: () => null,
+    getLatestContents: () => [],
 })
 
 export default ContentStoreContext
